767994e420136fdd8167b457eba005c2be9c9965
Lukáš Salfický <salficky.job@gmail.com>
2020-07-06 17:22:34 +0200

    Add table user identity and datacontext

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Data.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        public DbSet<UserIdentity> Identity { get; set; }
+    }
+}
---
+++ Data/DataModels/UserIdentity.cs
@@ -0,0 +1,11 @@
+﻿namespace Data.DataModels
+{
+    public class UserIdentity
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string PasswordHash { get; set; }
+    }
+}
