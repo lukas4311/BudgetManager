f51dd8daa1b9a70843ac36aa3dec7ea16197b831
Lukáš Salfický <salficky.job@gmail.com>
2020-07-07 18:43:09 +0200

    Add gulp task for sass transpilation.

---
+++ ManagerWeb/gulpfile.js
@@ -0,0 +1,12 @@
+﻿var gulp = require("gulp");
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./Sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./wwwroot/css/'));
+});
+
+gulp.task('watchsass', function () {
+    gulp.watch('./Sass/**/*.scss', gulp.series('sass'));
+});
