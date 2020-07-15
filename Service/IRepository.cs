c8f59c545a0cf1c73de1f461a1c5aa84de012618
Lukáš Salfický <salficky.job@gmail.com>
2020-07-15 17:18:46 +0200

    Add base generic repo.

---
+++ Service/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Repository
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> FindAll();
+
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+
+        void Create(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
---
+++ Service/Repository.cs
@@ -0,0 +1,44 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepository<T> where T : class
+    {
+        protected DataContext RepositoryContext { get; set; }
+
+        public RepositoryBase(DataContext repositoryContext)
+        {
+            this.RepositoryContext = repositoryContext;
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return this.RepositoryContext.Set<T>().AsNoTracking();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return this.RepositoryContext.Set<T>().Where(expression).AsNoTracking();
+        }
+
+        public void Create(T entity)
+        {
+            this.RepositoryContext.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            this.RepositoryContext.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            this.RepositoryContext.Set<T>().Remove(entity);
+        }
+    }
+}
