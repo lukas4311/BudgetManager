5f809e417bfa545b939cf2f60df96c91fbc22982
Lukáš Salfický <lukas4311@gmail.com>
2020-07-21 08:36:39 +0200

    Add ne payment fake records

--- ManagerWeb/Controllers/PaymentController.cs
+++ ManagerWeb/Controllers/PaymentController.cs
@@ -12,9 +12,12 @@ namespace ManagerWeb.Controllers
         {
             List<Payment> payments = new List<Payment>();
             payments.Add(new Payment { Amount = 150, Date = DateTime.Now, Id = 1, Name = "MC Donald", Description = "Nejaka utrata v MC Donald. Asi nejaky cheseburger." });
-            payments.Add(new Payment { Amount = 140, Date = DateTime.Now, Id = 2, Name = "bendas" });
-            payments.Add(new Payment { Amount = 109, Date = DateTime.Now, Id = 3, Name = "obed" });
-            payments.Add(new Payment { Amount = 549, Date = DateTime.Now, Id = 4, Name = "Decathlon", Description = "Kraviny asjdfkljadk lfjkla." });
+            payments.Add(new Payment { Amount = 140, Date = new DateTime(2020, 7, 19), Id = 2, Name = "bendas" });
+            payments.Add(new Payment { Amount = 109, Date = new DateTime(2020, 7, 12), Id = 3, Name = "obed" });
+            payments.Add(new Payment { Amount = 549, Date = new DateTime(2020, 7, 16), Id = 4, Name = "Decathlon", Description = "Kraviny asjdfkljadk lfjkla." });
+            payments.Add(new Payment { Amount = 2500, Date = new DateTime(2020, 7, 10), Id = 4, Name = "BTC" });
+            payments.Add(new Payment { Amount = 300, Date = new DateTime(2020, 7, 20), Id = 4, Name = "DM", Description = "Mliko." });
+            payments.Add(new Payment { Amount = 240, Date = new DateTime(2020, 7, 1), Id = 4, Name = "bendas" });
             return Json(payments);
         }
     }
