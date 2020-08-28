3e582c2f0f24edb3222b3d9764fabc49fc9be26e
Lukáš Salfický <lukas4311@gmail.com>
2020-08-28 08:12:08 +0200

    Add methods on tag controller

--- ManagerWeb/Controllers/TagController.cs
+++ ManagerWeb/Controllers/TagController.cs
@@ -5,12 +5,16 @@ using Repository;
 using System.Linq;
 using ManagerWeb.Models.DTOs;
 using System.Collections.Generic;
+using System;
+using Data.DataModels;
 
 namespace ManagerWeb.Controllers
 {
     [Authorize]
     public class TagController : Controller
     {
+        private const string AlreadyExist = "Tag with this code already exists";
+        private const string DoesntExists = "Tag doesn't exists";
         private readonly ILogger<HomeController> logger;
         private readonly ITagRepository tagRepository;
 
@@ -20,6 +24,7 @@ namespace ManagerWeb.Controllers
             this.tagRepository = tagRepository;
         }
 
+        [HttpGet]
         public JsonResult GetPaymentTags()
         {
             IEnumerable<TagModel> tags = this.tagRepository.FindAll().Select(t => new TagModel
@@ -29,6 +34,28 @@ namespace ManagerWeb.Controllers
             });
 
             return Json(new { tags });
+        }
+
+        [HttpPost]
+        public JsonResult AddTag(TagModel tagModel)
+        {
+            bool tagCodeExists = this.tagRepository.FindByCondition(t => string.Compare(t.Code, tagModel.Code, true) == 0).Any();
+
+            if (tagCodeExists)
+                throw new ArgumentException(AlreadyExist);
+
+            return Json(new {});
+        }
+
+        [HttpDelete]
+        public JsonResult DeleteTag(int tagId)
+        {
+            Tag tag = this.tagRepository.FindByCondition(t => t.Id == tagId).SingleOrDefault();
+
+            if (tag == null)
+                throw new ArgumentException(DoesntExists);
+
+            return Json(new {});
         }
     }
 }
