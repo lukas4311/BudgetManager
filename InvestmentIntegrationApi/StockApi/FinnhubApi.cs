e6b65f76f94d9b9a4cfc8b6a142b46ae46afbfb0
Lukáš Salfický <salficky.job@gmail.com>
2020-11-15 20:32:37 +0100

    Refatoring of stock service.

--- InvestmentIntegrationApi/StockApi/FinnhubApi.cs
+++ InvestmentIntegrationApi/StockApi/FinnhubApi.cs
@@ -2,6 +2,8 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using FinanceDataMining.StockApi.JsonModelDto;
+using System;
+using FinanceDataMining.Extensions;
 
 namespace FinanceDataMining.StockApi
 {
@@ -15,16 +17,21 @@ namespace FinanceDataMining.StockApi
             this.httpClient = httpClient;
         }
 
-        public async Task GetPreviousMonthCandles()
+        public async Task GetPreviousMonthCandles(string ticker)
         {
-            string res = await this.httpClient.GetStringAsync($"https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=1&from=1572651390&to=1572910590&token={Token}").ConfigureAwait(false);
-            CandleData candleData = JsonConvert.DeserializeObject<CandleData>(res);
+            await this.GetStockData(DateTime.Now, DateTime.Now.AddMonths(-1), ticker).ConfigureAwait(false);
         }
 
-        public async Task GetRealTimeQuoteData()
+        public async Task GetRealTimeQuoteData(string ticker)
         {
-            string res = await this.httpClient.GetStringAsync($"https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=1&from=1572651390&to=1572910590&token={Token}").ConfigureAwait(false);
-            QuoteData quoteData = JsonConvert.DeserializeObject<QuoteData>(res);
+            await this.GetStockData(DateTime.Now, DateTime.Now, ticker).ConfigureAwait(false);
+        }
+
+        private async Task GetStockData(DateTime from, DateTime to, string ticker)
+        {
+            string fetchedData = await this.httpClient.GetStringAsync($"https://finnhub.io/api/v1/stock/candle?symbol={ticker}&resolution=1&from={from.ConvertToUnixTimestamp()}&to={to.ConvertToUnixTimestamp()}&token={Token}").ConfigureAwait(false);
+
+            QuoteData quoteData = JsonConvert.DeserializeObject<QuoteData>(fetchedData);
         }
     }
 }
