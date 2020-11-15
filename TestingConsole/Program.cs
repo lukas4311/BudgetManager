13c5002008f82458e81cf5d0a71e0a7971030a83
Lukáš Salfický <salficky.job@gmail.com>
2020-11-15 19:54:34 +0100

    Add test console and add setting to data mining

---
+++ TestingConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using FinanceDataMining;
+using System;
+
+namespace TestingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SettingService testReadingConfig = new SettingService();
+            testReadingConfig.ReadConfig();
+        }
+    }
+}
