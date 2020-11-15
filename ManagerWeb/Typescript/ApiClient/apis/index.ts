ee0840d23275b83e7095f41231aaa6939886b981
Lukáš Salfický <salficky.job@gmail.com>
2020-11-15 10:28:20 +0100

    Add generated ope-api client for typescript

---
+++ ManagerWeb/Typescript/ApiClient/apis/index.ts
@@ -0,0 +1,5 @@
+export * from './BankAccountApi';
+export * from './BudgetApi';
+export * from './PaymentApi';
+export * from './TagApi';
+export * from './UserApi';
---
+++ ManagerWeb/Typescript/ApiClient/models/index.ts
@@ -0,0 +1,4 @@
+export * from './AddTagModel';
+export * from './BudgetModel';
+export * from './InlineObject';
+export * from './PaymentViewModel';
