10522679a5fb4477a431c497a1c1b1de05a9887f
Lukáš Salfický <salficky.job@gmail.com>
2020-11-17 15:59:43 +0100

    Add table for setting.

---
+++ Data/DataModels/EntityConfiguration/SettingConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.DataModels.EntityConfiguration
+{
+    public class SettingConfiguration : IEntityTypeConfiguration<Setting>
+    {
+        public void Configure(EntityTypeBuilder<Setting> builder)
+        {
+            builder.HasIndex(a => a.Code)
+                .IsUnique();
+        }
+    }
+}
---
+++ Data/DataModels/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace Data.DataModels
+{
+    public class Setting
+    {
+        public string Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string JsonSetting { get; set; }
+    }
+}
