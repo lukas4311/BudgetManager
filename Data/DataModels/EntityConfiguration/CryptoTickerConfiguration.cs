3354c90110001f61d070c42ca78733017f9b9f05
Lukáš Salfický <salficky.job@gmail.com>
2021-01-30 21:12:44 +0100

    Add entity setting

--- Data/DataModels/EntityConfiguration/CryptoTickerConfiguration.cs
+++ Data/DataModels/EntityConfiguration/CryptoTickerConfiguration.cs
@@ -5,8 +5,19 @@ namespace Data.DataModels.EntityConfiguration
 {
     public class CryptoTickerConfiguration : IEntityTypeConfiguration<CryptoTicker>
     {
+        private const int NameMaxLenght = 100;
+        private const int TickerMaxLenght = 20;
+
         public void Configure(EntityTypeBuilder<CryptoTicker> builder)
         {
+            builder.Property(b => b.Name)
+                .HasMaxLength(NameMaxLenght)
+                .IsRequired();
+
+            builder.Property(b => b.Ticker)
+                .HasMaxLength(TickerMaxLenght)
+                .IsRequired();
+
             builder.HasIndex(a => a.Ticker)
                 .IsUnique();
         }
