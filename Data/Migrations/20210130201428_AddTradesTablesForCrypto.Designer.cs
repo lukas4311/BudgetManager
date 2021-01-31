eaec144626e83a82ace473ff882c7d20326cc617
Lukáš Salfický <salficky.job@gmail.com>
2021-01-31 10:03:59 +0100

    Migratin for crypto tables

--- Data/Migrations/20210130201428_AddTradesTablesForCrypto.Designer.cs
+++ Data/Migrations/20210130201428_AddTradesTablesForCrypto.Designer.cs
@@ -4,14 +4,16 @@ using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210130201428_AddTradesTablesForCrypto")]
+    partial class AddTradesTablesForCrypto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20210130201428_AddTradesTablesForCrypto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddTradesTablesForCrypto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CryptoTicker",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ticker = table.Column<string>(maxLength: 20, nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CryptoTicker", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CryptoTradeHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TradeTimeStamp = table.Column<DateTime>(nullable: false),
+                    CryptoTickerId = table.Column<int>(nullable: false),
+                    TradeValue = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CryptoTradeHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CryptoTradeHistory_CryptoTicker_CryptoTickerId",
+                        column: x => x.CryptoTickerId,
+                        principalTable: "CryptoTicker",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CryptoTicker_Ticker",
+                table: "CryptoTicker",
+                column: "Ticker",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CryptoTradeHistory_CryptoTickerId",
+                table: "CryptoTradeHistory",
+                column: "CryptoTickerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CryptoTradeHistory");
+
+            migrationBuilder.DropTable(
+                name: "CryptoTicker");
+        }
+    }
+}
