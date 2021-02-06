80b53ddf9557ddfbd63f01492391bf1d872d1c72
Lukáš Salfický <salficky.job@gmail.com>
2021-02-06 11:06:56 +0100

    Add typescript apis

--- ManagerWeb/Typescript/ApiClient/apis/index.ts
+++ ManagerWeb/Typescript/ApiClient/apis/index.ts
@@ -1,5 +1,7 @@
 export * from './BankAccountApi';
 export * from './BudgetApi';
+export * from './CryptoApi';
+export * from './HomeApi';
 export * from './PaymentApi';
 export * from './TagApi';
 export * from './UserApi';
