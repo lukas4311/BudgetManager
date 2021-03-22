14b3c886cf83251c697c4a7b79e0bbb28a112340
Lukáš Salfický <salficky.job@gmail.com>
2021-03-22 20:42:30 +0100

    Prototype of own reworked menu component

--- ManagerWeb/Typescript/Components/Menu.tsx
+++ ManagerWeb/Typescript/Components/Menu.tsx
@@ -23,24 +23,10 @@ class Menu extends React.Component<{}, MenuState> {
     public render() {
 
         return (
-            // <nav className="border-bottom box-shadow">
-            //     <div className="flex flex-col spin circle button justify-center w-12 h-12" onClick={this.menuClick}>
-            //         <div className="w-3/5 bg-white mb-2 h-1 rounded-lg mx-auto"></div>
-            //         <div className="w-3/5 bg-white mb-2 h-1 rounded-lg mx-auto"></div>
-            //         <div className="w-3/5 bg-white h-1 rounded-lg mx-auto"></div>
-            //     </div>
-            //     <div className={(this.state.isClosed ? "containerClosed" : "containerOpen") + " menu absolute text-white rounded-lg bg-black mt-4 w-48 text-xl"}>
-            //         <a className="block mt-2 ml-10 cursor-pointer" onClick={_ => this.redirectToPage("")}>Home</a>
-            //         <a className="block mt-2 ml-10 cursor-pointer" onClick={_ => this.redirectToPage("payments")}>Platby</a>
-            //         <a className="block mt-2 ml-10 cursor-pointer" onClick={_ => this.redirectToPage("crypto-overview")}>Crypto</a>
-            //         <a className="block mt-2 ml-10 cursor-pointer" onClick={_ => this.redirectToPage("budget")}>Rozpočet</a>
-            //         <a className="block mt-2 ml-10 cursor-pointer" onClick={_ => this.redirectToPage("debts")}>Dluhy</a>
-            //         <a className="block mt-2 ml-10 mb-2 cursor-pointer" onClick={_ => this.redirectToPage("stats")}>Statistiky</a>
-            //     </div>
-            // </nav>
             <>
-                <input className="menu-toggler" id="menu-toggler" type="checkbox"></input>
-                <label htmlFor="menu-toggler"></label>
+                <a className="menuTogglerParent" onClick={this.menuClick}>
+                    <span className={"menu-toggler " + (this.state.isClosed ? "" : "checked") } id="menu-toggler"></span>
+                </a>
                 <ul>
                     <li className="menu-item">
                         <a className="fa fa-facebook" href="https://www.facebook.com/" target="_blank"></a>
--- ManagerWeb/wwwroot/js/menu.js
+++ ManagerWeb/wwwroot/js/menu.js
@@ -114,25 +114,9 @@ class Menu extends react_1.default.Component {
         this.setState((prev) => ({ isClosed: !prev.isClosed }));
     }
     render() {
-        return (
-        // <nav className="border-bottom box-shadow">
-        //     <div className="flex flex-col spin circle button justify-center w-12 h-12" onClick={this.menuClick}>
-        //         <div className="w-3/5 bg-white mb-2 h-1 rounded-lg mx-auto"></div>
-        //         <div className="w-3/5 bg-white mb-2 h-1 rounded-lg mx-auto"></div>
-        //         <div className="w-3/5 bg-white h-1 rounded-lg mx-auto"></div>
-        //     </div>
-        //     <div className={(this.state.isClosed ? "containerClosed" : "containerOpen") + " menu absolute text-white rounded-lg bg-black mt-4 w-48 text-xl"}>
-        //         <a className="block mt-2 ml-10 cursor-pointer" onClick={_ => this.redirectToPage("")}>Home</a>
-        //         <a className="block mt-2 ml-10 cursor-pointer" onClick={_ => this.redirectToPage("payments")}>Platby</a>
-        //         <a className="block mt-2 ml-10 cursor-pointer" onClick={_ => this.redirectToPage("crypto-overview")}>Crypto</a>
-        //         <a className="block mt-2 ml-10 cursor-pointer" onClick={_ => this.redirectToPage("budget")}>Rozpočet</a>
-        //         <a className="block mt-2 ml-10 cursor-pointer" onClick={_ => this.redirectToPage("debts")}>Dluhy</a>
-        //         <a className="block mt-2 ml-10 mb-2 cursor-pointer" onClick={_ => this.redirectToPage("stats")}>Statistiky</a>
-        //     </div>
-        // </nav>
-        react_1.default.createElement(react_1.default.Fragment, null,
-            react_1.default.createElement("input", { className: "menu-toggler", id: "menu-toggler", type: "checkbox" }),
-            react_1.default.createElement("label", { htmlFor: "menu-toggler" }),
+        return (react_1.default.createElement(react_1.default.Fragment, null,
+            react_1.default.createElement("a", { className: "menuTogglerParent", onClick: this.menuClick },
+                react_1.default.createElement("span", { className: "menu-toggler " + (this.state.isClosed ? "" : "checked"), id: "menu-toggler" })),
             react_1.default.createElement("ul", null,
                 react_1.default.createElement("li", { className: "menu-item" },
                     react_1.default.createElement("a", { className: "fa fa-facebook", href: "https://www.facebook.com/", target: "_blank" })),
