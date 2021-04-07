e6b466014951d6a39da4f55e3907db9b6ecb5385
Lukáš Salfický <salficky.job@gmail.com>
2021-04-07 20:41:16 +0200

    Add new files for actual budget

---
+++ ManagerWeb/Typescript/Components/Budget/ActualBudgetCard.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+class ActualBudgetCardProps{
+
+}
+
+export default class ActualBudgetCard extends React.Component<ActualBudgetCardProps, {}> {
+
+    constructor(props: ActualBudgetCardProps) {
+        super(props);
+    }
+
+    public render() {
+        return (
+            <React.Fragment>
+            </React.Fragment>
+        );
+    }
+}
