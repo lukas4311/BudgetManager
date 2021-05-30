480a4d6188ba658b423a2bf7695dacf61546978a
Lukáš Salfický <salficky.job@gmail.com>
2021-05-30 18:52:36 +0200

    Prepared metehod signature to get fear and greed

--- InvestmentIntegrationApi/CryproApi/FearAndGreed.cs
+++ InvestmentIntegrationApi/CryproApi/FearAndGreed.cs
@@ -1,12 +1,21 @@
-﻿namespace FinanceDataMining.CryproApi
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FinanceDataMining.CryproApi
 {
     public class FearAndGreed
     {
         private const string ApiUrl = "https://api.alternative.me/fng";
+        private readonly HttpClient httpClient;
 
-        public void GetActualFearAndGreed()
+        public FearAndGreed(HttpClient httpClient)
         {
+            this.httpClient = httpClient;
+        }
 
+        public async Task GetActualFearAndGreed()
+        {
+            string response = await this.httpClient.GetStringAsync(ApiUrl);
         }
     }
 }
