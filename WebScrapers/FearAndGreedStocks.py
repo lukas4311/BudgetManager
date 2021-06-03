5ce816454682795b5fbb999633eb53cd61edb51c
Lukáš Salfický <salficky.job@gmail.com>
2021-06-03 23:10:02 +0200

    Add logic to save data from web scrate to influxDB

--- WebScrapers/FearAndGreedStocks.py
+++ WebScrapers/FearAndGreedStocks.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+from datetime import datetime
+
 page = requests.get("https://money.cnn.com/data/fear-and-greed/")
 soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -14,3 +18,10 @@ startIndex =index + len(searchedString)
 
 fearAndGreedStocks = todayValue[startIndex:startIndex + 2];
 print(fearAndGreedStocks);
+
+bucket = "StockFearAndGreed"
+client = InfluxDBClient(url="http://localhost:8086", token="##TOKEN##", org="8f46f33452affe4a")
+
+write_api = client.write_api(write_options=SYNCHRONOUS)
+p = Point("fearAndGreed").field("value", float(fearAndGreedStocks)).time(datetime.utcnow(), WritePrecision.NS)
+write_api.write(bucket=bucket, record=p)
