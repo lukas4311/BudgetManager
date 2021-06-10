7389d712c43de398656c1dd7af06dedef47d5cb4
Lukáš Salfický <salficky.job@gmail.com>
2021-06-10 18:58:55 +0200

    Project namespace rename

--- FinancialDataProvider/Program.cs
+++ FinancialDataProvider/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace FinancialDataProvider
+namespace BudgetManager.FinancialDataProvider
 {
     public class Program
     {
