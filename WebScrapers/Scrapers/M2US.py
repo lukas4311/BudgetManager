bbe2fc33dc570679b43af42fa2a70ef07e51fe9a
Lukáš Salfický <salficky.job@gmail.com>
2021-06-17 22:00:07 +0200

    Prepare for saving M2 from US

---
+++ WebScrapers/Scrapers/M2US.py
@@ -0,0 +1,16 @@
+import requests
+from Services.InfluxRepository import InfluxRepository
+from configManager import token
+
+influx_repository = InfluxRepository("http://localhost:8086", "FinancialIndicators", token, "8f46f33452affe4a")
+data = requests.get("https://fred.stlouisfed.org/data/M2SL.txt")
+dataString = str(data.content)
+startIndex = dataString.index("VALUE")
+print(dataString[startIndex + len("VALUE"):])
+values = list(dataString[startIndex + len("VALUE\\r\\n"):].split("\\r\\n"))
+for m2Data in values:
+    splitValues = m2Data.split()
+
+    if len(splitValues) == 2:
+        print(splitValues[0])
+        print(splitValues[1])
