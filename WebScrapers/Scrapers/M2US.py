ad2b88f967b0dec1ce32fc99e620eee3ff674cd2
Lukáš Salfický <salficky.job@gmail.com>
2021-06-18 21:58:21 +0200

    Add saving M2 for US

--- WebScrapers/Scrapers/M2US.py
+++ WebScrapers/Scrapers/M2US.py
@@ -1,20 +1,34 @@
 import datetime
 
+import pytz
 import requests
+from influxdb_client import Point, WritePrecision
+
+from Models.MoneySupplyModel import MoneySupplyModel
 from Services.InfluxRepository import InfluxRepository
 from configManager import token
+m2Models = []
 
 influx_repository = InfluxRepository("http://localhost:8086", "FinancialIndicators", token, "8f46f33452affe4a")
 data = requests.get("https://fred.stlouisfed.org/data/M2SL.txt")
 dataString = str(data.content)
 startIndex = dataString.index("VALUE")
-print(dataString[startIndex + len("VALUE"):])
 values = list(dataString[startIndex + len("VALUE\\r\\n"):].split("\\r\\n"))
+
 for m2Data in values:
     splitValues = m2Data.split()
 
     if len(splitValues) == 2:
         dateString = splitValues[0].split("-")
         date = datetime.datetime(int(dateString[0]), int(dateString[1]), int(dateString[2]))
-        print(date)
-        print(float(splitValues[1]))
+        m2Models.append(MoneySupplyModel(float(splitValues[1]) * 1000000000, date))
+
+minDate = influx_repository.find_last("M2", "us")
+utc = pytz.UTC
+filtered = list(filter(lambda m: utc.localize(m.date) > minDate, m2Models))
+
+for m2 in filtered:
+    point = Point("M2").field("value", float(m2.value)).time(m2.date.astimezone(pytz.utc), WritePrecision.NS).tag("state", "us")
+    influx_repository.add(point)
+
+influx_repository.save()
