51edf1151ec10eb31614e43fd501a45f5e0146bb
Lukáš Salfický <salficky.job@gmail.com>
2021-06-20 19:42:00 +0200

    Money supply service refactoring

--- WebScrapers/Services/MoneySupplyUsService.py
+++ WebScrapers/Services/MoneySupplyUsService.py
@@ -14,19 +14,11 @@ class MoneySupplyUsService:
 
     def download_us_money_supply_data(self, fred_txt_link: str, measurement: str):
         m_models = []
-
-        data = requests.get(fred_txt_link)
-        data_string = str(data.content)
-        start_index = data_string.index("VALUE")
-        values = list(data_string[start_index + len("VALUE\\r\\n"):].split("\\r\\n"))
+        values = self.__parse_money_supply_data(fred_txt_link)
 
         for money_data in values:
             split_values = money_data.split()
-
-            if len(split_values) == 2:
-                date_string = split_values[0].split("-")
-                date = datetime.datetime(int(date_string[0]), int(date_string[1]), int(date_string[2]))
-                m_models.append(MoneySupplyModel(float(split_values[1]) * 1000000000, date))
+            self.__parse_data_from_string_array_to_money_supply_model(m_models, split_values)
 
         min_date = self.influx_repository.find_last(measurement, self.state)
         utc = pytz.UTC
@@ -34,6 +26,19 @@ class MoneySupplyUsService:
 
         self.add_all_to_repository(measurement, filtered)
         self.influx_repository.save()
+
+    def __parse_data_from_string_array_to_money_supply_model(self, m_models, split_values):
+        if len(split_values) == 2:
+            date_string = split_values[0].split("-")
+            date = datetime.datetime(int(date_string[0]), int(date_string[1]), int(date_string[2]))
+            m_models.append(MoneySupplyModel(float(split_values[1]) * 1000000000, date))
+
+    def __parse_money_supply_data(self, fred_txt_link):
+        data = requests.get(fred_txt_link)
+        data_string = str(data.content)
+        start_index = data_string.index("VALUE")
+        values = list(data_string[start_index + len("VALUE\\r\\n"):].split("\\r\\n"))
+        return values
 
     def __add_all_to_repository(self, measurement: str, filtered: list[MoneySupplyModel]):
         for moneySupply in filtered:
