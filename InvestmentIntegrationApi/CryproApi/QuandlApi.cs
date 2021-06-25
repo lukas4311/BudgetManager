e1ab037adccf2b877c3325b16691ac378087c6c0
Lukáš Salfický <salficky.job@gmail.com>
2021-06-25 18:00:32 +0200

    Preparing hash rate download

--- InvestmentIntegrationApi/CryproApi/QuandlApi.cs
+++ InvestmentIntegrationApi/CryproApi/QuandlApi.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading.Tasks;
 using BudgetManager.FinanceDataMining.Models;
+using System.Text.Json;
 
 namespace BudgetManager.FinanceDataMining.CryproApi
 {
     class QuandlApi
     {
-        private const string goldDataUrl = "https://www.quandl.com/api/v3/datasets/BCHAIN/HRATE.json?api_key=";
+        private const string hashRateDataUrl = "https://www.quandl.com/api/v3/datasets/BCHAIN/HRATE.json?api_key=";
         private readonly HttpClient httpClient;
         private readonly string apiKey;
 
@@ -17,9 +19,18 @@ namespace BudgetManager.FinanceDataMining.CryproApi
             this.apiKey = apiKey;
         }
 
-        public IEnumerable<HashRateData> GetHashRateData()
+        public async Task<IEnumerable<(DateTime, decimal)>> GetHashRateData(DateTime fromDate)
         {
+            HashRateResponse hashRateData = await this.ReqeuestHashRateData(fromDate);
+
             throw new NotImplementedException();
         }
+
+        private async Task<HashRateResponse> ReqeuestHashRateData(DateTime from)
+        {
+            string data = await this.httpClient.GetStringAsync($"{hashRateDataUrl}{this.apiKey}&{from:yyyy-MM-dd}");
+            return JsonSerializer.Deserialize<HashRateResponse>(data);
+        }
+
     }
 }
