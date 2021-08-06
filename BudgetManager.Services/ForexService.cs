0d3c920ef273242ac4293fc3b2c2454dc35d3d20
Lukáš Salfický <salficky.job@gmail.com>
2021-08-06 23:19:54 +0200

    Forex service reworked

--- BudgetManager.Services/ForexService.cs
+++ BudgetManager.Services/ForexService.cs
@@ -1,4 +1,5 @@
-﻿using BudgetManager.Services.Contracts;
+﻿using BudgetManager.InfluxDbData;
+using BudgetManager.Services.Contracts;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,22 +8,21 @@ namespace BudgetManager.Services
 {
     public class ForexService : IForexService
     {
-        //private const string bucketForex = "Forex";
-        //private const string organizationId = "8f46f33452affe4a";
-        //private readonly IRepository<ForexData> forexRepository;
+        private const string bucketForex = "Forex";
+        private const string organizationId = "8f46f33452affe4a";
+        private readonly IRepository<ForexData> forexRepository;
 
-        //public ForexService(IRepository<ForexData> forexRepository)
-        //{
-        //    this.forexRepository = forexRepository;
-        //}
+        public ForexService(IRepository<ForexData> forexRepository)
+        {
+            this.forexRepository = forexRepository;
+        }
 
-        //public async Task<double> GetCurrentExchangeRate(string fromSymbol, string toSymbol)
-        //{
-        //    DataSourceIdentification dataSourceIdentification = new DataSourceIdentification(organizationId, bucketForex);
-        //    List<ForexData> data = await this.forexRepository.GetLastWrittenRecordsTime(dataSourceIdentification);
-        //    return data.SingleOrDefault(a => string.Equals(a.BaseCurrency, fromSymbol, System.StringComparison.OrdinalIgnoreCase)
-        //        && string.Equals(a.Currency, toSymbol, System.StringComparison.OrdinalIgnoreCase))?.Price ?? 0;
-        //}
-        public Task<double> GetCurrentExchangeRate(string fromSymbol, string toSymbol) => throw new System.NotImplementedException();
+        public async Task<double> GetCurrentExchangeRate(string fromSymbol, string toSymbol)
+        {
+            DataSourceIdentification dataSourceIdentification = new DataSourceIdentification(organizationId, bucketForex);
+            List<ForexData> data = await this.forexRepository.GetLastWrittenRecordsTime(dataSourceIdentification);
+            return data.SingleOrDefault(a => string.Equals(a.BaseCurrency, fromSymbol, System.StringComparison.OrdinalIgnoreCase)
+                && string.Equals(a.Currency, toSymbol, System.StringComparison.OrdinalIgnoreCase))?.Price ?? 0;
+        }
     }
 }
