63a8e3a14f84402f696f259c87997f61d89ee54d
Lukáš Salfický <salficky.job@gmail.com>
2021-08-07 18:51:42 +0200

    Reworked all services

---
+++ BudgetManager.Domain/DTOs/PaymentDeleteModel.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+using BudgetManager.Data.DataModels;
+
+namespace BudgetManager.Domain.DTOs
+{
+    public record PaymentDeleteModel(BankAccount bankAccount, IEnumerable<InterestRate> interests, IEnumerable<Payment> payments, IEnumerable<PaymentTag> paymentTags);
+}
