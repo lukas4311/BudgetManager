3c9d42318fca13b40ccd921061e8b95ea53634c1
Lukáš Salfický <salficky.job@gmail.com>
2021-08-17 16:56:12 +0200

    Add new controller for base API.

---
+++ BudgetManager.Api/Controllers/BudgetController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using BudgetManager.Domain.DTOs;
+using BudgetManager.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetManager.Api.Controllers
+{
+    [ApiController]
+    [Route("budget")]
+    public class BudgetController : ControllerBase
+    {
+        private readonly IBudgetService budgetService;
+
+        public BudgetController(IBudgetService budgetService)
+        {
+            this.budgetService = budgetService;
+        }
+
+        [HttpGet("getAll")]
+        public ActionResult<IEnumerable<BudgetModel>> Get()
+        {
+            return Ok(this.budgetService.Get());
+        }
+
+        [HttpGet("get")]
+        public ActionResult<BudgetModel> Get(int id)
+        {
+            return Ok(this.budgetService.Get(id));
+        }
+
+        [HttpGet("getActual")]
+        public ActionResult<IEnumerable<BudgetModel>> GetActual()
+        {
+            return Ok(this.budgetService.GetActual());
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add([FromBody] BudgetModel budgetModel)
+        {
+            this.budgetService.Add(budgetModel);
+            return Ok();
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromBody] BudgetModel budgetModel)
+        {
+            this.budgetService.Update(budgetModel);
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromBody] int id)
+        {
+            this.budgetService.Delete(id);
+            return Ok();
+        }
+    }
+}
