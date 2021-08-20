269aaa1cdd2d1d4943f142b980dbb5202febab90
Lukáš Salfický <salficky.job@gmail.com>
2021-08-20 20:09:10 +0200

    Add new controller to auth API to verify user

---
+++ BudgetManager.AuthApi/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using BudgetManager.AuthApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetManager.AuthApi.Controllers
+{
+    [Route("auth")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(UserModel model)
+        {
+            // TODO: 
+            // 1. call user service to verify user name and password
+            // 2. if true -> generate token, return generated token 
+            // 1. if false return 400
+
+            return BadRequest(new { message = "Username or password is incorrect" });
+        }
+    }
+}
