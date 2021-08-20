2a5685d4d5fdcbe234690d7816de908e081b83c7
Lukáš Salfický <salficky.job@gmail.com>
2021-08-20 20:30:41 +0200

    Add user service to authenticate user

---
+++ BudgetManager.Services.Contracts/IUserService.cs
@@ -0,0 +1,9 @@
+﻿namespace BudgetManager.Services.Contracts
+{
+    public interface IUserService
+    {
+        bool Authenticate(string username, string password);
+
+        int GetUserId(string userLogin);
+    }
+}
---
+++ BudgetManager.Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using BudgetManager.Repository;
+using System.Linq;
+
+namespace BudgetManager.Services
+{
+    public class UserService
+    {
+        private readonly IUserIdentityRepository userIdentityRepository;
+
+        public UserService(IUserIdentityRepository userIdentityRepository)
+        {
+            this.userIdentityRepository = userIdentityRepository;
+        }
+
+        public bool Authenticate(string username, string passwordHash) => this.userIdentityRepository.FindByCondition(x => x.Login == username && x.PasswordHash == passwordHash).Count() == 1;
+
+        public int GetUserId(string userLogin) => this.userIdentityRepository.FindByCondition(a => a.Login == userLogin).Single().Id;
+    }
+}
