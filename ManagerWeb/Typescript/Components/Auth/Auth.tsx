ab938711d21eb855b861ba2fca80e45179a4403a
Lukáš Salfický <salficky.job@gmail.com>
2021-10-13 18:00:40 +0200

    Add new SPA form for login

--- ManagerWeb/Typescript/Components/Auth/Auth.tsx
+++ ManagerWeb/Typescript/Components/Auth/Auth.tsx
@@ -1,7 +1,32 @@
 ﻿import React from "react";
 import ReactDOM from "react-dom";
+import { AuthApi } from "../../ApiClient/Auth";
 
-class Auth extends React.Component<{}, {}>{
+class AuthState {
+    login: string;
+    password: string;
+}
+
+class Auth extends React.Component<{}, AuthState>{
+
+    constructor(state: AuthState) {
+        super(state);
+    }
+
+    private login = () => {
+        let authApi: AuthApi = new AuthApi();
+        authApi.authAuthenticatePost({ userModel: { password: this.state.password, userName: this.state.login } });
+    }
+
+    private onChangeLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let login = e.target.value;
+        this.setState({ login: login });
+    }
+
+    private onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let pass = e.target.value;
+        this.setState({ password: pass });
+    }
 
     render = () => {
         return (
@@ -12,19 +37,19 @@ class Auth extends React.Component<{}, {}>{
                     <div className="flex">
                         <div className="w-1/2">
                             <div className="relative inline-block w-2/3 m-auto">
-                                <input className="effect-11 w-full" placeholder="Login" />
+                                <input className="effect-11 w-full" placeholder="Login" value={this.state.login} onChange={e => this.onChangeLogin(e)} />
                                 <span className="focus-bg"></span>
                             </div>
                         </div>
                         <div className="w-1/2">
                             <div className="relative inline-block w-2/3 m-auto">
-                                <input type="password" className="effect-11 w-full" placeholder="Heslo" />
+                                <input type="password" className="effect-11 w-full" placeholder="Heslo" value={this.state.password} onChange={(e) => this.onChangePassword(e)} />
                                 <span className="focus-bg"></span>
                             </div>
                         </div>
                     </div>
                     <div className="flex mt-8">
-                        <button type="submit" className="m-auto bg-vermilion px-4 py-1 rounded-sm hover:text-vermilion hover:bg-white duration-500">Potvrdit</button>
+                        <button onClick={this.login} className="m-auto bg-vermilion px-4 py-1 rounded-sm hover:text-vermilion hover:bg-white duration-500">Potvrdit</button>
                     </div>
                 </form>
             </div>
