b1fe9d7d2d9868a44ec5fc446460abcf047a6376
Lukas Salficky <lukas4311@gmail.com>
2021-11-06 14:54:18 +0100

    Fixed problem with auth middleware

--- BudgetManager.Api/Middlewares/JwtMiddleware.cs
+++ BudgetManager.Api/Middlewares/JwtMiddleware.cs
@@ -19,53 +19,82 @@ namespace BudgetManager.Api.Middlewares
     {
         private readonly RequestDelegate next;
         private readonly AuthApiSetting appSettings;
+        private readonly HttpClient httpClient;
 
         public JwtMiddleware(RequestDelegate next, IOptions<AuthApiSetting> appSettings)
         {
             this.next = next;
             this.appSettings = appSettings.Value;
+            this.httpClient = new HttpClient();
         }
 
         public async Task Invoke(HttpContext context)
         {
             string token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
 
-            if (token != null)
+            if (token == null)
             {
-                await attachUserToContext(context, token);
-            }
-            else
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-                await context.Response.WriteAsync("Unauthorized");
+                await this.SetUnauthorizedResponse(context);
                 return;
             }
 
+            bool tokenIsValid = await this.ValidateToken(token);
+
+            if (!tokenIsValid)
+            {
+                await this.SetUnauthorizedResponse(context);
+                return;
+            }
+
+            await this.AttachUserToContext(context, token);
             await next(context);
         }
 
-        private async Task attachUserToContext(HttpContext context, string token)
+        private async Task SetUnauthorizedResponse(HttpContext context)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            await context.Response.WriteAsync("Unauthorized");
+        }
+
+        private async Task<bool> ValidateToken(string token)
+        {
+            HttpClient client = new HttpClient();
+            string bodyData = JsonSerializer.Serialize(new { Token = token });
+            StringContent data = new StringContent(bodyData, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(this.appSettings.ValidateUrl, data);
+            bool isValid = false;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                isValid = JsonSerializer.Deserialize<bool>(result);
+            }
+
+            return isValid;
+        }
+
+        private async Task AttachUserToContext(HttpContext context, string token)
         {
             try
             {
                 HttpClient client = new HttpClient();
-                string bodyData = JsonSerializer.Serialize(new { Token = token });
-                StringContent data = new StringContent(bodyData, Encoding.UTF8, "application/json");
-                HttpResponseMessage response = await client.PostAsync(this.appSettings.ValidateUrl, data);
-                bool isValid = false;
+                //string bodyData = JsonSerializer.Serialize(new { Token = token });
+                //StringContent data = new StringContent(bodyData, Encoding.UTF8, "application/json");
+                //HttpResponseMessage response = await client.PostAsync(this.appSettings.ValidateUrl, data);
+                //bool isValid = false;
 
-                if (response.IsSuccessStatusCode)
-                {
-                    string result = response.Content.ReadAsStringAsync().Result;
-                    isValid = JsonSerializer.Deserialize<bool>(result);
-                }
+                //if (response.IsSuccessStatusCode)
+                //{
+                //    string result = response.Content.ReadAsStringAsync().Result;
+                //    isValid = JsonSerializer.Deserialize<bool>(result);
+                //}
 
-                if (isValid)
-                {
-                    string responseUserData = await client.GetStringAsync($"{this.appSettings.DataUrl}?token={token}");
-                    UserDataModel user = JsonSerializer.Deserialize<UserDataModel>(responseUserData);
-                    this.SignIn(context, user.userName, user.userId);
-                }
+                //if (!isValid)
+                //    return;
+
+                string responseUserData = await client.GetStringAsync($"{this.appSettings.DataUrl}?token={token}");
+                UserDataModel user = JsonSerializer.Deserialize<UserDataModel>(responseUserData);
+                this.SignIn(context, user.userName, user.userId);
             }
             catch (Exception e)
             {
