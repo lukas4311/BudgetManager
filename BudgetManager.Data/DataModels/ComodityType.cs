69ea7f49de6ec949959b1aef0b3bff3508ec177e
Lukas Salficky <lukas4311@gmail.com>
2021-12-10 17:26:20 +0100

    Add tables definiction for comodities

---
+++ BudgetManager.Data/DataModels/ComodityType.cs
@@ -0,0 +1,13 @@
+﻿namespace BudgetManager.Data.DataModels
+{
+    public class ComodityType
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ComodityUnit ComodityUnit { get; set; }
+
+        public int ComodityUnitId { get; set; }
+    }
+}
---
+++ BudgetManager.Data/DataModels/ComodityUnit.cs
@@ -0,0 +1,11 @@
+﻿namespace BudgetManager.Data.DataModels
+{
+    public class ComodityUnit
+    {
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+    }
+}
