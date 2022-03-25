84fbcfba76b7b59b0daa5d031d768e518a5a46e1
Lukas Salficky <lukas4311@gmail.com>
2022-03-25 22:22:21 +0100

    Add model for company profile.

--- BudgetManager.WebScrapers/Scrapers/fmpApi.py
+++ BudgetManager.WebScrapers/Scrapers/fmpApi.py
@@ -2,6 +2,53 @@ import requests
 from secret import fmpApiToken
 from typing import List
 import datetime
+from typing import Any
+from dataclasses import dataclass
+import json
+
+
+@dataclass
+class CompanyProfile:
+    symbol: str
+    price: float
+    beta: float
+    volAvg: int
+    mktCap: float
+    lastDiv: float
+    range: str
+    changes: float
+    companyName: str
+    currency: str
+    cik: str
+    isin: str
+    cusip: str
+    exchange: str
+    exchangeShortName: str
+    industry: str
+    website: str
+    description: str
+    ceo: str
+    sector: str
+    country: str
+    fullTimeEmployees: str
+    phone: str
+    address: str
+    city: str
+    state: str
+    zip: str
+    dcfDiff: float
+    dcf: float
+    image: str
+    ipoDate: str
+    defaultImage: bool
+    isEtf: bool
+    isActivelyTrading: bool
+    isAdr: bool
+    isFund: bool
+
+    @staticmethod
+    def create_from_json(data):
+        return CompanyProfile(**data)
 
 
 class FmpApiService:
@@ -14,7 +61,9 @@ class FmpApiService:
 
     def get_company_profile(self, ticker: str):
         response = requests.get(f'{self.fmpUri}/api/v3/profile/{ticker}?apikey={self.token}')
-        print(response.json())
+        jsonData = response.json()
+        jsonObj = jsonData[0]
+        return CompanyProfile.create_from_json(jsonObj)
 
     def get_historical_dividend(self, ticker: str):
         response = requests.get(f'{self.fmpUri}/api/v3/historical-price-full/stock_dividend/{ticker}?apikey={self.token}')
@@ -47,3 +96,29 @@ class FmpScraper:
 
     def __init__(self):
         self.fmp_service = FmpApiService(fmpApiToken)
+
+    def download_profile(self):
+        profile = self.fmp_service.get_company_profile("AAPL")
+        print(profile.companyName)
+        print(profile.price)
+        print(profile.description)
+        print(profile.exchangeShortName)
+        print(profile.state)
+        print(profile.sector)
+        print(profile.ceo)
+        print(profile.address)
+        print(profile.image)
+        print(profile.defaultImage)
+        print(profile.mktCap)
+        print(profile.isin)
+        print(profile.currency)
+        print(profile.industry)
+        print(profile.city)
+        print(profile.country)
+        print(profile.symbol)
+        print(profile.website)
+        print(profile.volAvg)
+
+
+fmpScraper = FmpScraper()
+fmpScraper.download_profile()
