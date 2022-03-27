b6f3d6cf6cc5b5d102d2030ffac31cb29e192b96
Lukas Salficky <lukas4311@gmail.com>
2022-03-27 16:58:56 +0200

    Add configuration of new entities.

---
+++ BudgetManager.Data/DataModels/EntityConfiguration/AddressConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BudgetManager.Data.DataModels.EntityConfiguration
+{
+    public class AddressConfiguration : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.Property(p => p.Country).HasMaxLength(50).IsRequired();
+        }
+    }
+}
---
+++ BudgetManager.Data/DataModels/EntityConfiguration/CompanyProfileConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BudgetManager.Data.DataModels.EntityConfiguration
+{
+    public class CompanyProfileConfiguration : IEntityTypeConfiguration<CompanyProfile>
+    {
+        public void Configure(EntityTypeBuilder<CompanyProfile> builder)
+        {
+            builder.Property(p => p.Symbol).HasMaxLength(10).IsRequired();
+            builder.Property(p => p.Currency).HasMaxLength(10).IsRequired();
+            builder.Property(p => p.CompanyName).HasMaxLength(50).IsRequired();
+
+            builder.HasOne<Address>().WithMany().HasForeignKey(p => p.AddressId);
+        }
+    }
+}
