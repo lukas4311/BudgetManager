61ac78e9b2c68778d81af74d1f63ec38b3f04097
Lukas Salficky <lukas4311@gmail.com>
2022-06-04 11:05:25 +0200

    Add core lib for configure Serilog and ELK.

--- BudgetManager.Api/Program.cs
+++ BudgetManager.Api/Program.cs
@@ -1,12 +1,10 @@
 using Autofac.Extensions.DependencyInjection;
+using BudgetManager.WebCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.Elasticsearch;
 using System;
-using System.Reflection;
 
 namespace BudgetManager.Api
 {
@@ -14,18 +12,18 @@ namespace BudgetManager.Api
     {
         public static void Main(string[] args)
         {
-            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{environment}.json", optional: true)
-                .Build();
-
-            ConfigureLogging(environment, configuration);
-
             try
             {
                 Log.Information("Starting Web Host");
-                CreateHostBuilder(args).Build().Run();
+                Host.CreateDefaultBuilder(args)
+                .ConfigureHostWithSerilogToElk()
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .Build()
+                .Run();
             }
             catch (Exception ex)
             {
@@ -35,37 +33,6 @@ namespace BudgetManager.Api
             {
                 Log.CloseAndFlush();
             }
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSerilog()
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-
-        private static void ConfigureLogging(string environment, IConfigurationRoot configuration)
-        {
-            string elaticUrl = configuration.GetValue<string>("Elk:ElasticUrl");
-            string index = configuration.GetValue<string>("Elk:ElasaticIndexKey");
-
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                .Enrich.FromLogContext()
-                .Enrich.WithEnvironmentName()
-                .Enrich.WithMachineName()
-                .WriteTo.Console()
-                .WriteTo.Debug()
-                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elaticUrl))
-                {
-                    AutoRegisterTemplate = true,
-                    AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
-                    IndexFormat = $"{index.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
-                })
-                .ReadFrom.Configuration(configuration)
-                .CreateLogger();
         }
     }
 }
---
+++ BudgetManager.WebCore/SerilogExtension.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
+
+namespace BudgetManager.WebCore
+{
+    public static class SerilogExtension
+    {
+        private static readonly string ElkSection = "Elk";
+        private static readonly string ElasticUrl = "ElasticUrl";
+        private static readonly string ElasticIndex = "ElasaticIndexKey";
+
+        public static IHostBuilder ConfigureHostWithSerilogToElk(this IHostBuilder host)
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true)
+                .Build();
+
+            ConfigureLogging(environment, configuration);
+
+            host = host.UseSerilog();
+            return host;
+        }
+
+        private static void ConfigureLogging(string environment, IConfigurationRoot configuration)
+        {
+            string elaticUrl = configuration.GetValue<string>($"{ElkSection}:{ElasticUrl}");
+            string index = configuration.GetValue<string>($"{ElkSection}:{ElasticIndex}");
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .Enrich.WithEnvironmentName()
+                .Enrich.WithMachineName()
+                .WriteTo.Console()
+                .WriteTo.Debug()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elaticUrl))
+                {
+                    AutoRegisterTemplate = true,
+                    AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+                    IndexFormat = $"{index.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+                })
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+    }
+}
