d8d0019e8ef84bd28fd6af0dfdc75f11617eb5f9
Lukas Salficky <lukas4311@gmail.com>
2023-01-08 12:11:02 +0100

    Yahoo csv parsing from download link

--- BudgetManager.WebScrapers/Services/YahooService.py
+++ BudgetManager.WebScrapers/Services/YahooService.py
@@ -1,5 +1,13 @@
 import urllib.request
+import csv
+import io
 
 with urllib.request.urlopen("https://query1.finance.yahoo.com/v7/finance/download/KO?period1=511056000&period2=1673156073&interval=1d&events=history&includeAdjustedClose=true") as url:
     data = url.read().decode()
-    print(data)
+
+    rows = csv.DictReader(io.StringIO(data))
+    for row in rows:
+        close = row["Close"]
+        date = row["Date"]
+        print(date)
+        print(close)
