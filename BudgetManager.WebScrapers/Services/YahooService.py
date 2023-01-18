68d273146ea7bc1de53a7f95550c8df7d4de49fc
Lukas Salficky <lukas4311@gmail.com>
2023-01-18 20:44:27 +0100

    Yahoo service solved time range for data download.

--- BudgetManager.WebScrapers/Services/YahooService.py
+++ BudgetManager.WebScrapers/Services/YahooService.py
@@ -10,11 +10,11 @@ class YahooService:
     closeValueKey = "4. close"
     token: str
 
-    def get_stock_price_history(self, ticker: str):
+    def get_stock_price_history(self, ticker: str, unix_from: str, unix_to: str):
         print('Downloading stock history for: ' + ticker)
         stockPriceData = []
-        with urllib.request.urlopen(
-                f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1=511056000&period2=1673156073&interval=1d&events=history&includeAdjustedClose=true') as url:
+        # with urllib.request.urlopen(f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1=511056000&period2=1673156073&interval=1d&events=history&includeAdjustedClose=true') as url:
+        with urllib.request.urlopen(f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={unix_from}&period2={unix_to}&interval=1d&events=history&includeAdjustedClose=true') as url:
             data = url.read().decode()
             rows = csv.DictReader(io.StringIO(data))
             for row in rows:
@@ -23,8 +23,16 @@ class YahooService:
                 pandas_date = pd.to_datetime(date)
                 pandas_date = pandas_date.tz_localize("Europe/Prague")
                 pandas_date = pandas_date.tz_convert("utc")
-                priceModel = StockPriceData(pandas_date, float(close))
+                close_price = 0
+                try:
+                    close_price = float(close)
+                except ValueError:
+                    continue
+
+                priceModel = StockPriceData(pandas_date, close_price)
                 stockPriceData.append(priceModel)
 
         return stockPriceData
 
+# yahooService = YahooService()
+# yahooService.get_stock_price_history("MMM")
