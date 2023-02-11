e81ef06aa017dd8bfef926612c5c4f880c2a63d2
Lukas Salficky <lukas4311@gmail.com>
2023-02-11 13:46:54 +0100

    Add file for company profiile component

---
+++ ManagerWeb/Typescript/Components/Stocks/CompanyProfile.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+export const CompanyProfile = (props: {}) => {
+    return (
+        <div>
+            <h2 className="text-xl font-semibold">Company profile</h2>
+            <div>
+
+            </div>
+        </div>
+    );
+}
