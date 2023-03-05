55f18bbe623d28ef0fd042b8d572bc488d6abbf6
Lukas Salficky <lukas4311@gmail.com>
2023-03-05 18:55:05 +0100

    Created bank service

---
+++ ManagerWeb/Typescript/Services/BankAccountService.ts
@@ -0,0 +1,45 @@
+import { BankAccountApi } from "../ApiClient/Main/apis";
+import { BankAccount, BankAccountModel } from "../ApiClient/Main/models";
+import BankAccountViewModel from "../Model/BankAccountViewModel";
+
+export default class BankAccountService {
+    private bankAccountApi: BankAccountApi;
+
+    constructor(bankAccountApi: BankAccountApi) {
+        this.bankAccountApi = bankAccountApi;
+    }
+
+    public async getAllBankAccounts(): Promise<BankAccountViewModel[]> {
+        let bankAccounts: BankAccountModel[] = await this.bankAccountApi.bankAccountsAllGet();
+        let bankViewModels: BankAccountViewModel[] = this.getMappedViewModels(bankAccounts);
+        return bankViewModels;
+    }
+
+    public async updateBankAccount(model: BankAccountViewModel) {
+        let bankModel: BankAccount = {
+            code: model.code, id: model.id, openingBalance: parseInt(model.openingBalance.toString())
+        };
+
+        await this.bankAccountApi.bankAccountsPut({ bankAccountModel: bankModel });
+    }
+
+    public async createBankAccount(model: BankAccountViewModel) {
+        let bankModel: BankAccount = {
+            code: model.code, id: model.id, openingBalance: parseInt(model.openingBalance.toString())
+        };
+
+        await this.bankAccountApi.bankAccountsPost({ bankAccountModel: bankModel });
+    }
+
+    private getMappedViewModels = (bankAccountModels: BankAccountModel[]): BankAccountViewModel[] =>
+        bankAccountModels.map(b => this.mapDataModelToViewModel(b));
+
+
+    private mapDataModelToViewModel = (bankAccountModels: BankAccountModel): BankAccountViewModel => {
+        let viewModel = new BankAccountViewModel();
+        viewModel.code = bankAccountModels.code;
+        viewModel.id = bankAccountModels.id;
+        viewModel.openingBalance = bankAccountModels.openingBalance;
+        return viewModel;
+    }
+}
