65c2dcd5978153cf44225c8db428ed73110179b9
Lukas Salficky <lukas4311@gmail.com>
2023-03-12 17:59:26 +0100

    Add crypto service

---
+++ ManagerWeb/Typescript/Services/CryptoService.ts
@@ -0,0 +1,10 @@
+import { CryptoApiInterface } from "../ApiClient/Main";
+
+export default class CryptoService {
+    private cryptoApi: CryptoApiInterface;
+
+    constructor(cryptoApi: CryptoApiInterface) {
+        this.cryptoApi = cryptoApi;
+    }
+
+}
