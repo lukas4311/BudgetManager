c7c58040393dc458cf7a9148baf4f6e56c75b2a1
Lukas Salficky <lukas4311@gmail.com>
2023-10-10 19:42:40 +0200

    Prepare for donwloading of data for all ticker from source file.

--- BudgetManager.WebScrapers/Scrapers/Final_StockSplitHistoryScraper.py
+++ BudgetManager.WebScrapers/Scrapers/Final_StockSplitHistoryScraper.py
@@ -1,6 +1,10 @@
 from Services.YahooService import YahooService
+from SourceFiles.stockList import stockToDownload
 
+tickersToScrape = stockToDownload
 yahooService = YahooService()
-split_data = yahooService.get_stock_split_history('AMZN', '511056000', '1696896000')
 
-print(split_data)
+for ticker in tickersToScrape:
+     split_data = yahooService.get_stock_split_history(ticker, '511056000', '1696896000')
+     print(split_data)
+
--- BudgetManager.WebScrapers/SourceFiles/stockList.py
+++ BudgetManager.WebScrapers/SourceFiles/stockList.py
@@ -1,3 +1,3 @@
-stockToDownload = ['SE', 'KWEB', 'CNYA', 'ABNB', 'BABA' 'O', 'NEST', 'MDLZ', 'UPST', 'SIE', 'META', 'TSM', 'TLRY',
+stockToDownload = ['SE', 'KWEB', 'CNYA', 'ABNB', 'BABA', 'NEST', 'MDLZ', 'UPST', 'SIE', 'META', 'TSM', 'TLRY',
                    'SIE.DE', 'NESN', 'VUAA.L', 'AAPL', 'AMZN', 'MSFT', 'O', 'GOOGL', 'BAC', 'BRK', 'KO', 'CVS', 'DLR',
                    'LMT', 'MCD', 'META', 'NVDA', 'TSM', 'TSLA', 'UPST', 'GOOG', 'VUSA']
