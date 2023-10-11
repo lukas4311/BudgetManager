e989424a5d7b7b4d8dfb39e6c513eea41a45f6fb
Salfický Lukáš <Lukas.Salficky@dako-cz.cz>
2023-10-11 20:32:28 +0200

    Add timestamp of split.

--- BudgetManager.WebScrapers/Orm/StockSplit.py
+++ BudgetManager.WebScrapers/Orm/StockSplit.py
@@ -1,5 +1,6 @@
-from sqlalchemy import String, Numeric, Integer
+from sqlalchemy import String, Numeric, Integer, DateTime
 from sqlalchemy.orm import mapped_column, DeclarativeBase, Mapped
+import datetime
 
 
 class Base(DeclarativeBase):
@@ -10,5 +11,6 @@ class StockSplit(Base):
     __tablename__ = 'StockSplit'
     id: Mapped[int] = mapped_column(primary_key=True)
     stockTickerId: Mapped[int] = mapped_column(Integer, nullable=False)
+    splitTimeStamp: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), nullable=False)
     splitTextInfo: Mapped[str] = mapped_column(String, nullable=False)
     splitCoefficient: Mapped[float] = mapped_column(Numeric, nullable=False)
