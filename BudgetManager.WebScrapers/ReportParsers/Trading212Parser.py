5b5104031f30d3589638ceef3a150ab216feed73
Salfický Lukáš <Lukas.Salficky@dako-cz.cz>
2024-01-25 22:41:25 +0100

    Prepared for stock report processing

--- BudgetManager.WebScrapers/ReportParsers/Trading212Parser.py
+++ BudgetManager.WebScrapers/ReportParsers/Trading212Parser.py
@@ -1,12 +1,16 @@
+import base64 as b64
 import csv
-from datetime import datetime
-from dataclasses import dataclass
-import pandas as pd
+import io
 import logging
-import pyodbc
-import secret
-# from sqlalchemy import create_engine
 import warnings
+from dataclasses import dataclass
+from datetime import datetime
+
+import pandas as pd
+
+from ReportParsers.CoinbaseParser import ParseCsvError
+from Services.DB.StockRepository import StockRepository
+
 warnings.filterwarnings('ignore', category=UserWarning)
 
 log_name = 'Logs/trading212.' + datetime.now().strftime('%Y-%m-%d') + '.log'
@@ -50,50 +54,68 @@ class Trading212ReportParser:
                     print(ticker + " - error - " + e)
         return parsedData
 
+    def map_csv_row_to_model(self, row):
+        action = row["Action"]
+        time = row["Time"]
+        ticker = row["Ticker"]
+        name = row["Name"]
+        number_of_shares = row["No. of shares"]
+        total = float(row["Total (CZK)"])
+
+        if action == "Market buy":
+            total = total * -1
+
+        pandas_date = pd.to_datetime(time)
+        pandas_date = pandas_date.tz_localize("Europe/Prague")
+        pandas_date = pandas_date.tz_convert("utc")
+
+        return TradingReportData(pandas_date, ticker, name, number_of_shares, total)
+
     def save_report_data_to_db(self):
         data = self.read_report_csv_file()
 
         for tradeData in data:
             try:
-                # print(tradeData)
-                self.store_orders(tradeData)
+                print(tradeData)
+                # self.store_orders(tradeData)
             except Exception as e:
                 logging.info(
                     'Error while saving for ticker: ' + tradeData.ticker + 'and for time' + tradeData.time.strftime(
                         '%Y-%m-%d'))
                 logging.error(e)
 
-    def store_orders(self, tradingData: TradingReportData):
-        conn = pyodbc.connect(
-            f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={secret.serverName};DATABASE={secret.datebaseName};Trusted_Connection=yes;')
-        stock_ticker_sql = """SELECT [Id] FROM [dbo].[StockTicker] WHERE [Ticker] = ?"""
+def process_report_data(stock_repo: StockRepository, parser: Trading212ReportParser):
+    broker_report_data = stock_repo.get_all_crypto_broker_reports_to_process()
+    all_reports_data = []
 
-        ticker_data = pd.read_sql_query(stock_ticker_sql, conn, params=[tradingData.ticker])
+    for report_data in broker_report_data:
+        try:
+            parse_report_data_to_model(all_reports_data, parser, report_data)
+        except Exception as e:
+            stock_repo.changeProcessState(report_data.id, "ParsingError")
 
-        if len(ticker_data.index) == 0:
-            cursor = conn.cursor()
-            params = (tradingData.ticker, tradingData.name)
-            cursor.execute('''
-                                INSERT INTO [dbo].[StockTicker]([Ticker], [Name])
-                                VALUES(?,?)
-                            ''', params)
-            conn.commit()
+    print(all_reports_data)
+    for parsed_report in all_reports_data:
+        try:
+            stock_repo.store_trade_data(parsed_report["data"])
+            stock_repo.changeProcessState(parsed_report["report_id"], "Finished")
+        except Exception as e:
+            print(parsed_report)
+            stock_repo.changeProcessState(parsed_report["report_id"], "SavinggError")
 
-        ticker_data = pd.read_sql_query(stock_ticker_sql, conn, params=[tradingData.ticker])
-        stock_ticker_id = int(ticker_data['Id'].values[0])
 
-        currency_sql = """SELECT Id from CurrencySymbol where SYMBOL = 'CZK'"""
-        currency_data = pd.read_sql(currency_sql, conn)
-        currency_id: int = int(currency_data['Id'].values[0])
+def parse_report_data_to_model(all_reports_data, parser, report_data):
+    try:
+        parsed_csv = b64.b64decode(report_data.fileContentBase64).decode('utf-8')
+        rows = csv.DictReader(io.StringIO(parsed_csv))
+        records = []
+        for row in rows:
+            coinbase_record = parser.map_csv_row_to_model(row)
+            records.append(coinbase_record)
 
-        cursor = conn.cursor()
-        params = (tradingData.time.strftime('%Y-%m-%d'), stock_ticker_id, float(tradingData.number_of_shares),
-                  float(tradingData.total), currency_id)
-        cursor.execute('''
-                            INSERT INTO [dbo].[StockTradeHistory]([TradeTimeStamp],[StockTickerId],[TradeSize],[TradeValue],[CurrencySymbolId],[UserIdentityId])
-                            VALUES (?,?,?,?,?,1)
-                        ''', params)
-        conn.commit()
+        all_reports_data.append({"user_id": report_data.userIdentityId, "report_id": report_data.id, "data": records})
+    except Exception as e:
+        raise ParseCsvError("Error while parsing CSV")
 
 
 parser = Trading212ReportParser()
