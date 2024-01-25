b03c7cd7f322655ed25575873e8bf33bb7a82b30
Salfický Lukáš <Lukas.Salficky@dako-cz.cz>
2024-01-25 22:12:10 +0100

    Stock repo

--- BudgetManager.WebScrapers/Services/DB/StockRepository.py
+++ BudgetManager.WebScrapers/Services/DB/StockRepository.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, select, insert
+from sqlalchemy import create_engine, select, insert, and_
 from sqlalchemy.orm import Session
 
 import secret
@@ -7,9 +7,11 @@ from Orm.BrokerReportToProcessState import BrokerReportToProcessState
 from Orm.BrokerReportType import BrokerReportType
 from Orm.CurrencySymbol import CurrencySymbol
 from Orm.StockTicker import Base, StockTicker
-
+from Orm.StockTradeHistory import StockTradeHistory
+from ReportParsers.Trading212Parser import TradingReportData
 
 connectionString = f'mssql+pyodbc://@{secret.serverName}/{secret.datebaseName}?driver=ODBC+Driver+17+for+SQL+Server&Trusted_Connection=yes'
+
 
 class StockRepository:
     def get_ticker_id(self, ticker: str):
@@ -64,3 +66,42 @@ class StockRepository:
         session.close()
 
         return broker_report_data
+
+    def insert_stock_trade(self, tradingData: TradingReportData, currency_code: str):
+        print("Insert record")
+
+        ticker_id = int(self.get_ticker_id(tradingData.ticker))
+
+        if ticker_id is None:
+            print('Throw exception')
+
+        currency_id = int(self.get_currency_id(currency_code))
+
+        if ticker_id is None:
+            print('Throw exception')
+
+        engine = create_engine(connectionString)
+
+        Base.metadata.create_all(engine)
+        session = Session(engine)
+        crypto_trade = (select(StockTradeHistory)
+                        .where(and_(StockTradeHistory.tradeValue == tradingData.total,
+                                    StockTradeHistory.tradeSize == tradingData.number_of_shares
+                                    , StockTradeHistory.stockTickerId == ticker_id
+                                    , StockTradeHistory.tradeTimeStamp == tradingData.time)
+                               ))
+        crypto_data = session.scalars(crypto_trade)
+        crypto_trade = crypto_data.first()
+
+        if crypto_trade is None:
+            insert_command = insert(StockTradeHistory).values(tradeTimeStamp=tradingData.time, stockTickerId=ticker_id,
+                                                              tradeSize=tradingData.number_of_shares,
+                                                              tradeValue=tradingData.total,
+                                                              currencySymbolId=currency_id, userIdentityId=1)
+            with engine.connect() as conn:
+                conn.execute(insert_command)
+                conn.commit()
+        else:
+            print('Trade is already saved.')
+
+        session.close()
