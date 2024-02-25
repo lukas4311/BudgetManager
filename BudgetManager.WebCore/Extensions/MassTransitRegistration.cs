8f14f19b22c5a53d1a569a92f78d42c2d8adfab1
Salfický Lukáš <Lukas.Salficky@dako-cz.cz>
2024-02-25 16:34:21 +0100

    Mass transit registration extension

---
+++ BudgetManager.WebCore/Extensions/MassTransitRegistration.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BudgetManager.WebCore.Extensions
+{
+    public static class MassTransitRegistration
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, RabbitMqConfig rabbitMqConfig)
+        {
+            services.AddMassTransit(x =>
+            {
+                x.UsingRabbitMq((ctx, cfg) =>
+                {
+                    cfg.Host(rabbitMqConfig.RabbitMqUri, "/", c =>
+                    {
+                        c.Username(rabbitMqConfig.User);
+                        c.Password(rabbitMqConfig.Pass);
+                    });
+                    cfg.ConfigureEndpoints(ctx);
+                });
+            });
+            return services;
+        }
+    }
+}
---
+++ BudgetManager.WebCore/RabbitMqConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace BudgetManager.WebCore
+{
+    public class RabbitMqConfig
+    {
+        public Uri RabbitMqUri { get; set; }
+
+        public string User { get; set; }
+
+        public string Pass { get; set; }
+    }
+}
