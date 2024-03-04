da03dd32a26c990cfb624234801709a581c866ad
Lukas Salficky <lukas4311@gmail.com>
2024-03-04 19:31:28 +0100

    Add new script to consume rabbit mq messages for new ticker

---
+++ BudgetManager.WebScrapers/MessageBrokerConsumers/NewTickerConsumer.py
@@ -0,0 +1,14 @@
+import pika
+
+def recieve_new_ticker(ch, method, properties, body):
+    print(f" [x] Received {body}")
+
+queue_name = 'test'
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.queue_declare(queue=queue_name, durable=True)
+channel.basic_consume(queue=queue_name, on_message_callback=recieve_new_ticker, auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
+
