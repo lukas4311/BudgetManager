38c2a6d71d66dad3a7ea0df9bc30225b55762c71
Lukas Salficky <lukas4311@gmail.com>
2024-04-20 09:32:49 +0200

    Migration to renamed user id column

---
+++ BudgetManager.Data/Migrations/20240420073049_RenamedUserIdColumn.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetManager.Data.Migrations
+{
+    public partial class RenamedUserIdColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notification_UserIdentity_UserId",
+                table: "Notification");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Notification",
+                newName: "UserIdentityId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Notification_UserId",
+                table: "Notification",
+                newName: "IX_Notification_UserIdentityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notification_UserIdentity_UserIdentityId",
+                table: "Notification",
+                column: "UserIdentityId",
+                principalTable: "UserIdentity",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notification_UserIdentity_UserIdentityId",
+                table: "Notification");
+
+            migrationBuilder.RenameColumn(
+                name: "UserIdentityId",
+                table: "Notification",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Notification_UserIdentityId",
+                table: "Notification",
+                newName: "IX_Notification_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notification_UserIdentity_UserId",
+                table: "Notification",
+                column: "UserId",
+                principalTable: "UserIdentity",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
