e870b9ebf2ce2c9bb1beba057cd33b8eef45796b
Lukas Salficky <lukas4311@gmail.com>
2024-04-25 22:16:47 +0200

    Add comments for core project

--- BudgetManager.Core/Extensions/StringExtension.cs
+++ BudgetManager.Core/Extensions/StringExtension.cs
@@ -3,24 +3,46 @@ using System;
 
 namespace BudgetManager.Core.Extensions
 {
-    public static class StringExtension
+    /// <summary>
+/// Provides extension methods for string objects to parse Unix time seconds to DateTime.
+/// </summary>
+public static class StringExtension
+{
+    /// <summary>
+    /// Parses a Unix time seconds string to a UTC DateTime.
+    /// </summary>
+    /// <param name="unixTimeSeconds">The Unix time seconds string to parse.</param>
+    /// <returns>A DateTime object representing the time in UTC.</returns>
+    /// <exception cref="BadUnixTimeFormat">Thrown when the format of Unix time seconds cannot be parsed.</exception>
+    public static DateTime ParseToUtcDateTime(this string unixTimeSeconds)
     {
-        public static DateTime ParseToUtcDateTime(this string unixTimeSeconds)
-        {
-            DateTime convertedUnixSeconds = ConvertUnixTimeSeconds(unixTimeSeconds);
-            return DateTime.SpecifyKind(convertedUnixSeconds, DateTimeKind.Utc); ;
-        }
-        public static DateTime ParseToLocalDateTime(this string unixTimeSeconds)
-        {
-            return ConvertUnixTimeSeconds(unixTimeSeconds);
-        }
-
-        private static DateTime ConvertUnixTimeSeconds(string unixTimeSeconds)
-        {
-            if (!long.TryParse(unixTimeSeconds, out long unixTimeSecondsParsed))
-                throw new BadUnixTimeFormat($"Format of uniix time seconds cannot be parsed. {unixTimeSeconds}");
-
-            return DateTimeOffset.FromUnixTimeSeconds(unixTimeSecondsParsed).DateTime;
-        }
+        DateTime convertedUnixSeconds = ConvertUnixTimeSeconds(unixTimeSeconds);
+        return DateTime.SpecifyKind(convertedUnixSeconds, DateTimeKind.Utc);
     }
+
+    /// <summary>
+    /// Parses a Unix time seconds string to a local DateTime.
+    /// </summary>
+    /// <param name="unixTimeSeconds">The Unix time seconds string to parse.</param>
+    /// <returns>A DateTime object representing the time in the local time zone.</returns>
+    /// <exception cref="BadUnixTimeFormat">Thrown when the format of Unix time seconds cannot be parsed.</exception>
+    public static DateTime ParseToLocalDateTime(this string unixTimeSeconds)
+    {
+        return ConvertUnixTimeSeconds(unixTimeSeconds);
+    }
+
+    /// <summary>
+    /// Converts a Unix time seconds string to a DateTime object.
+    /// </summary>
+    /// <param name="unixTimeSeconds">The Unix time seconds string to convert.</param>
+    /// <returns>A DateTime object representing the specified time.</returns>
+    /// <exception cref="BadUnixTimeFormat">Thrown when the format of Unix time seconds cannot be parsed.</exception>
+    private static DateTime ConvertUnixTimeSeconds(string unixTimeSeconds)
+    {
+        if (!long.TryParse(unixTimeSeconds, out long unixTimeSecondsParsed))
+            throw new BadUnixTimeFormat($"Format of Unix time seconds cannot be parsed: {unixTimeSeconds}");
+
+        return DateTimeOffset.FromUnixTimeSeconds(unixTimeSecondsParsed).DateTime;
+    }
+}
 }
