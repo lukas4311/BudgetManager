c48926a7ec86a55fc446619cfd91dd0a16025eb7
Lukas Salficky <lukas4311@gmail.com>
2024-05-17 19:55:35 +0200

    Add notification orm

---
+++ BudgetManager.WebScrapers/Orm/Notification.py
@@ -0,0 +1,18 @@
+from sqlalchemy import String, Integer, DateTime, Boolean
+from sqlalchemy.orm import mapped_column, DeclarativeBase, Mapped
+import datetime
+
+class Base(DeclarativeBase):
+    pass
+
+
+class CurrencySymbol(Base):
+    __tablename__ = 'Notification'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    userIdentityId: Mapped[int] = mapped_column(Integer, nullable=False)
+    heading: Mapped[str] = mapped_column(String, nullable=False)
+    content: Mapped[str] = mapped_column(String, nullable=False)
+    isDisplayed: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    timestamp: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    attachmentUrl: Mapped[str] = mapped_column(String, nullable=False)
