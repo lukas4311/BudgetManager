90ea752a45bb757c7d348239e4bab23d736fa07c
lukas4311 <lukas4311@gmail.com>
2024-06-02 13:12:56 +0200

    Comments for models and exceptions

--- BudgetManager.Domain/MessagingContracts/TickerRequest.cs
+++ BudgetManager.Domain/MessagingContracts/TickerRequest.cs
@@ -1,8 +1,17 @@
 ﻿namespace BudgetManager.Domain.MessagingContracts;
 
+/// <summary>
+/// Model proevent message of new stock ticker request
+/// </summary>
 public class TickerRequest
 {
+    /// <summary>
+    /// Stock ticker
+    /// </summary>
     public string Ticker { get; set; }
 
+    /// <summary>
+    /// User Id
+    /// </summary>
     public int UserId { get; set; }
 }
--- BudgetManager.Domain/Models/JwtSetting.cs
+++ BudgetManager.Domain/Models/JwtSetting.cs
@@ -1,3 +1,8 @@
 ﻿namespace BudgetManager.Domain.Models;
 
+/// <summary>
+/// Model for JWT setting
+/// </summary>
+/// <param name="SecretKey">Secure key</param>
+/// <param name="ExpireMinutes">Expiration period in minutes</param>
 public record JwtSetting(string SecretKey, int ExpireMinutes);
