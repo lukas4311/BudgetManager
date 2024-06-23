5a00e39e4e3fd590ab15c61f22eec5abfe07334a
Lukas Salficky <lukas4311@gmail.com>
2024-06-23 15:30:44 +0200

    Enum item and enum item type entity

---
+++ BudgetManager.Data/DataModels/EnumItem.cs
@@ -0,0 +1,33 @@
+﻿namespace BudgetManager.Data.DataModels
+{
+    /// <summary>
+    /// Enumaration item
+    /// </summary>
+    public class EnumItem : IDataModel
+    {
+        /// <summary>
+        /// Gets or sets the ID of enum item.
+        /// </summary>
+        public int Id { get ; set; }
+
+        /// <summary>
+        /// Gets or sets the code representing enum item.
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of enum item.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of enum item type ID
+        /// </summary>
+        public int EnumItemTypeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of enum item type
+        /// </summary>
+        public EnumItemType EnumItemType { get; set; }
+    }
+}
---
+++ BudgetManager.Data/DataModels/EnumItemType.cs
@@ -0,0 +1,20 @@
+﻿namespace BudgetManager.Data.DataModels
+{
+    public class EnumItemType : IDataModel
+    {
+        /// <summary>
+        /// Gets or sets the ID of enum item type.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the code representing enum item type.
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of enum item type.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
