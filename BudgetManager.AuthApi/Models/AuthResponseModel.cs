e395fdba3efa18f38c2f96900ed16895f36314bc
Lukas Salficky <lukas4311@gmail.com>
2024-06-26 17:35:03 +0200

    Comments

--- BudgetManager.AuthApi/Models/AuthResponseModel.cs
+++ BudgetManager.AuthApi/Models/AuthResponseModel.cs
@@ -1,4 +1,10 @@
 ﻿namespace BudgetManager.AuthApi.Models
 {
+    /// <summary>
+    /// Auth response model
+    /// </summary>
+    /// <param name="Token">Access token</param>
+    /// <param name="UserId">User ID</param>
+    /// <param name="UserName">User name</param>
     public record AuthResponseModel(string Token, int UserId, string UserName);
 }
--- BudgetManager.AuthApi/Models/UserModel.cs
+++ BudgetManager.AuthApi/Models/UserModel.cs
@@ -1,9 +1,18 @@
 ﻿namespace BudgetManager.AuthApi.Models
 {
+    /// <summary>
+    /// User model
+    /// </summary>
     public class UserModel
     {
+        /// <summary>
+        /// User login
+        /// </summary>
         public string UserName { get; set; }
 
+        /// <summary>
+        /// User pass
+        /// </summary>
         public string Password { get; set; }
     }
 }
