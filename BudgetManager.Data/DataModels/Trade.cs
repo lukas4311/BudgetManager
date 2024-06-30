cf4e5647205565f41cc1c1b1998c8c83a14402c1
Lukas Salficky <lukas4311@gmail.com>
2024-06-30 18:33:32 +0200

    Prepare for general trade table to remove specialized trades tables (stock, crypto and comodity). Need to merge them togother cause same structure.

---
+++ BudgetManager.Data/DataModels/Trade.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace BudgetManager.Data.DataModels
+{
+    public class Trade : IDataModel
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier of the stock trade history entry.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the timestamp when the trade occurred.
+        /// </summary>
+        public DateTime TradeTimeStamp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identifier of the associated stock ticker.
+        /// </summary>
+        public int TickerId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the associated stock ticker object.
+        /// </summary>
+        public EnumItem Ticker { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the trade (e.g., number of shares).
+        /// </summary>
+        public double TradeSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of the trade.
+        /// </summary>
+        public double TradeValue { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identifier of the currency symbol used for the trade.
+        /// </summary>
+        public int TradeCurrencySymbolId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the currency symbol object used for the trade.
+        /// </summary>
+        public EnumItem TradeCurrencySymbol { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identifier of the user associated with the trade.
+        /// </summary>
+        public int UserIdentityId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user identity object associated with the trade.
+        /// </summary>
+        public UserIdentity UserIdentity { get; set; }
+    }
+}
