21973a0eb586530489a0ed7d044037c8b3ebedf0
Lukas Salficky <lukas4311@gmail.com>
2024-07-07 15:28:28 +0200

    Fixed comodity trades to have all columns

--- ManagerWeb/Scripts/ComodityTrade.sql
+++ ManagerWeb/Scripts/ComodityTrade.sql
@@ -19,8 +19,8 @@ ORDER BY
 
 
 --**** monthly grouped and accumulated
-DECLARE @FromDate DATE = '2021-01-01';  -- Replace with your actual from date
-DECLARE @ToDate DATE = '2024-12-31';    -- Replace with your actual to date
+DECLARE @FromDate DATE = '2020-01-01';  -- Replace with your actual from date
+DECLARE @ToDate DATE = '2023-12-31';    -- Replace with your actual to date
 
 ;WITH MonthSeries AS (
     SELECT 
@@ -37,52 +37,85 @@ ComodityTypes AS (
     SELECT DISTINCT ComodityTypeId
     FROM [dbo].[ComodityTradeHistory]
 ),
-CurrencySymbols AS (
-    SELECT DISTINCT CurrencySymbolId
-    FROM [dbo].[ComodityTradeHistory]
-),
-Trades AS (
-    SELECT
+UsedCurrencySymbols AS (
+    SELECT DISTINCT
         cth.ComodityTypeId,
-        cth.CurrencySymbolId,
-        cth.TradeTimeStamp,
-        CASE WHEN cth.TradeValue >= 0 THEN -cth.TradeSize ELSE cth.TradeSize END AS AdjustedTradeSize,
-        cth.TradeValue,
-        YEAR(cth.TradeTimeStamp) AS TradeYear,
-        MONTH(cth.TradeTimeStamp) AS TradeMonth
+        cths.Id as CurrencySymbolId,
+        cths.Symbol AS CurrencySymbol
     FROM
         [dbo].[ComodityTradeHistory] cth
+    JOIN
+        [dbo].[CurrencySymbol] cths ON cth.CurrencySymbolId = cths.Id
+    WHERE
+        cth.TradeTimeStamp BETWEEN @FromDate AND @ToDate
 ),
-AggregatedTrades AS (
+MonthYearCrossJoin AS (
     SELECT
         ct.ComodityTypeId,
-        cs.CurrencySymbolId,
+        ucs.CurrencySymbolId,
+        ucs.CurrencySymbol,
         YEAR(ms.MonthStart) AS TradeYear,
-        MONTH(ms.MonthStart) AS TradeMonth,
-        ISNULL(SUM(t.AdjustedTradeSize), 0) AS TradeSize,
-        ISNULL(SUM(t.TradeValue), 0) AS TradeValue
+        MONTH(ms.MonthStart) AS TradeMonth
     FROM
         MonthSeries ms
     CROSS JOIN
         ComodityTypes ct
     CROSS JOIN
-        CurrencySymbols cs
+        UsedCurrencySymbols ucs
+    WHERE
+        EXISTS (
+            SELECT 1
+            FROM [dbo].[ComodityTradeHistory] cth
+            WHERE cth.ComodityTypeId = ct.ComodityTypeId
+            AND cth.CurrencySymbolId = ucs.CurrencySymbolId
+            AND cth.TradeTimeStamp BETWEEN @FromDate AND @ToDate
+        )
+),
+AggregatedTrades AS (
+    SELECT
+        my.ComodityTypeId,
+        my.CurrencySymbolId,
+        my.CurrencySymbol,
+        my.TradeYear,
+        my.TradeMonth,
+        ISNULL(SUM(t.AdjustedTradeSize), 0) AS TradeSize,
+        ISNULL(SUM(t.TradeValue), 0) AS TradeValue
+    FROM
+        MonthYearCrossJoin my
     LEFT JOIN
-        Trades t ON 
-            ct.ComodityTypeId = t.ComodityTypeId AND
-            cs.CurrencySymbolId = t.CurrencySymbolId AND
-            t.TradeYear = YEAR(ms.MonthStart) AND 
-            t.TradeMonth = MONTH(ms.MonthStart)
+        (
+            SELECT
+                cth.ComodityTypeId,
+                cth.CurrencySymbolId,
+                cths.Symbol AS CurrencySymbol,
+                cth.TradeTimeStamp,
+                CASE WHEN cth.TradeValue >= 0 THEN -cth.TradeSize ELSE cth.TradeSize END AS AdjustedTradeSize,
+                cth.TradeValue,
+                YEAR(cth.TradeTimeStamp) AS TradeYear,
+                MONTH(cth.TradeTimeStamp) AS TradeMonth
+            FROM
+                [dbo].[ComodityTradeHistory] cth
+            JOIN
+                [dbo].[CurrencySymbol] cths ON cth.CurrencySymbolId = cths.Id
+            WHERE
+                cth.TradeTimeStamp BETWEEN @FromDate AND @ToDate
+        ) t ON 
+            my.ComodityTypeId = t.ComodityTypeId AND
+            my.CurrencySymbolId = t.CurrencySymbolId AND
+            my.TradeYear = t.TradeYear AND 
+            my.TradeMonth = t.TradeMonth
     GROUP BY
-        ct.ComodityTypeId,
-        cs.CurrencySymbolId,
-        YEAR(ms.MonthStart),
-        MONTH(ms.MonthStart)
+        my.ComodityTypeId,
+        my.CurrencySymbolId,
+        my.CurrencySymbol,
+        my.TradeYear,
+        my.TradeMonth
 ),
 AccumulatedTrades AS (
     SELECT
         ComodityTypeId,
         CurrencySymbolId,
+        CurrencySymbol,
         TradeYear,
         TradeMonth,
         SUM(TradeSize) OVER (PARTITION BY ComodityTypeId, CurrencySymbolId ORDER BY TradeYear, TradeMonth) AS AccumulatedTradeSize,
@@ -93,15 +126,13 @@ AccumulatedTrades AS (
 SELECT
     at.ComodityTypeId,
     at.CurrencySymbolId,
-    cs.Symbol,
+    at.CurrencySymbol,
     at.TradeYear,
     at.TradeMonth,
     at.AccumulatedTradeSize,
     at.AccumulatedTradeValue
 FROM
     AccumulatedTrades at
-JOIN
-    [dbo].[CurrencySymbol] cs ON at.CurrencySymbolId = cs.Id
 ORDER BY
     at.ComodityTypeId, at.CurrencySymbolId, at.TradeYear, at.TradeMonth
-OPTION (MAXRECURSION 0)
+OPTION (MAXRECURSION 0);
