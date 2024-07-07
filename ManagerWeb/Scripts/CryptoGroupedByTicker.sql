d29786760019ddbeb2c7a69dccee4efda1141937
Lukas Salficky <lukas4311@gmail.com>
2024-07-07 14:59:55 +0200

    Probably final selects for crypto

--- ManagerWeb/Scripts/CryptoGroupedByTicker.sql
+++ ManagerWeb/Scripts/CryptoGroupedByTicker.sql
@@ -2,40 +2,52 @@
 ;WITH AdjustedTrades AS (
     SELECT
         cth.CryptoTickerId,
+        cth.CurrencySymbolId,
+        cths.Symbol,
         cth.TradeTimeStamp,
         CASE WHEN cth.TradeValue >= 0 THEN -cth.TradeSize ELSE cth.TradeSize END AS AdjustedTradeSize,
         cth.TradeValue
     FROM
         [dbo].[CryptoTradeHistory] cth
+    JOIN
+        [dbo].[CurrencySymbol] cths ON cth.CurrencySymbolId = cths.Id
 ),
 AggregatedTrades AS (
     SELECT
-        cth.CryptoTickerId,
-        SUM(CASE WHEN cth.TradeValue >= 0 THEN -cth.TradeSize ELSE cth.TradeSize END) AS TotalTradeSize,
-        SUM(cth.TradeValue) AS TotalTradeValue
+        at.CryptoTickerId,
+        at.CurrencySymbolId,
+        at.Symbol,
+        SUM(CASE WHEN at.TradeValue >= 0 THEN -at.AdjustedTradeSize ELSE at.AdjustedTradeSize END) AS TotalTradeSize,
+        SUM(at.TradeValue) AS TotalTradeValue
     FROM
-        [dbo].[CryptoTradeHistory] cth
+        AdjustedTrades at
     GROUP BY
-        cth.CryptoTickerId
+        at.CryptoTickerId,
+        at.CurrencySymbolId,
+        at.Symbol
 ),
 AccumulatedTrades AS (
     SELECT
         CryptoTickerId,
+        CurrencySymbolId,
+        Symbol,
         TotalTradeSize,
         TotalTradeValue,
-        SUM(TotalTradeSize) OVER (PARTITION BY CryptoTickerId) AS AccumulatedTradeSize
+        SUM(TotalTradeSize) OVER (PARTITION BY CryptoTickerId ORDER BY CryptoTickerId) AS AccumulatedTradeSize
     FROM
         AggregatedTrades
 )
 SELECT
     CryptoTickerId,
+    CurrencySymbolId,
+    Symbol,
     TotalTradeSize,
     TotalTradeValue,
     AccumulatedTradeSize
 FROM
     AccumulatedTrades
 ORDER BY
-    CryptoTickerId
+    CryptoTickerId, CurrencySymbolId;
 
 
 --**** grouped by trade date
@@ -69,7 +81,7 @@ ORDER BY
     CryptoTickerId, TradeTimeStamp;
 
 --**** gouped by ticker and month
-DECLARE @FromDate DATE = '2022-01-01';  -- Replace with your actual from date
+DECLARE @FromDate DATE = '2021-01-01';  -- Replace with your actual from date
 DECLARE @ToDate DATE = '2023-12-31';    -- Replace with your actual to date
 
 ;WITH MonthSeries AS (
@@ -87,52 +99,98 @@ CryptoTickers AS (
     SELECT DISTINCT CryptoTickerId
     FROM [dbo].[CryptoTradeHistory]
 ),
-Trades AS (
-    SELECT
+UsedCurrencySymbols AS (
+    SELECT DISTINCT
         cth.CryptoTickerId,
-        cth.TradeTimeStamp,
-        CASE WHEN cth.TradeValue >= 0 THEN -cth.TradeSize ELSE cth.TradeSize END AS AdjustedTradeSize,
-        cth.TradeValue,
-        YEAR(cth.TradeTimeStamp) AS TradeYear,
-        MONTH(cth.TradeTimeStamp) AS TradeMonth
+        cths.Id AS CurrencySymbolId,
+        cths.Symbol AS CurrencySymbol
     FROM
         [dbo].[CryptoTradeHistory] cth
+    JOIN
+        [dbo].[CurrencySymbol] cths ON cth.CurrencySymbolId = cths.Id
+    WHERE
+        cth.TradeTimeStamp BETWEEN @FromDate AND @ToDate
 ),
-AggregatedTrades AS (
+MonthYearCrossJoin AS (
     SELECT
         ct.CryptoTickerId,
+        ucs.CurrencySymbolId,
+        ucs.CurrencySymbol,
         YEAR(ms.MonthStart) AS TradeYear,
-        MONTH(ms.MonthStart) AS TradeMonth,
-        ISNULL(SUM(t.AdjustedTradeSize), 0) AS TradeSize,
-        ISNULL(SUM(t.TradeValue), 0) AS TradeValue
+        MONTH(ms.MonthStart) AS TradeMonth
     FROM
         MonthSeries ms
     CROSS JOIN
         CryptoTickers ct
+    CROSS JOIN
+        UsedCurrencySymbols ucs
+    WHERE
+        EXISTS (
+            SELECT 1
+            FROM [dbo].[CryptoTradeHistory] cth
+            WHERE cth.CryptoTickerId = ct.CryptoTickerId
+            AND cth.CurrencySymbolId = ucs.CurrencySymbolId
+            AND cth.TradeTimeStamp BETWEEN @FromDate AND @ToDate
+        )
+),
+AggregatedTrades AS (
+    SELECT
+        my.CryptoTickerId,
+        my.CurrencySymbolId,
+        my.CurrencySymbol,
+        my.TradeYear,
+        my.TradeMonth,
+        ISNULL(SUM(t.AdjustedTradeSize), 0) AS TradeSize,
+        ISNULL(SUM(t.TradeValue), 0) AS TradeValue
+    FROM
+        MonthYearCrossJoin my
     LEFT JOIN
-        Trades t ON 
-            ct.CryptoTickerId = t.CryptoTickerId AND
-            t.TradeYear = YEAR(ms.MonthStart) AND 
-            t.TradeMonth = MONTH(ms.MonthStart)
+        (
+            SELECT
+                cth.CryptoTickerId,
+                cth.CurrencySymbolId,
+                cths.Symbol AS CurrencySymbol,
+                cth.TradeTimeStamp,
+                CASE WHEN cth.TradeValue >= 0 THEN -cth.TradeSize ELSE cth.TradeSize END AS AdjustedTradeSize,
+                cth.TradeValue,
+                YEAR(cth.TradeTimeStamp) AS TradeYear,
+                MONTH(cth.TradeTimeStamp) AS TradeMonth
+            FROM
+                [dbo].[CryptoTradeHistory] cth
+            JOIN
+                [dbo].[CurrencySymbol] cths ON cth.CurrencySymbolId = cths.Id
+            WHERE
+                cth.TradeTimeStamp BETWEEN @FromDate AND @ToDate
+        ) t ON 
+            my.CryptoTickerId = t.CryptoTickerId AND
+            my.CurrencySymbolId = t.CurrencySymbolId AND
+            my.TradeYear = t.TradeYear AND 
+            my.TradeMonth = t.TradeMonth
     GROUP BY
-        ct.CryptoTickerId,
-        YEAR(ms.MonthStart),
-        MONTH(ms.MonthStart)
+        my.CryptoTickerId,
+        my.CurrencySymbolId,
+        my.CurrencySymbol,
+        my.TradeYear,
+        my.TradeMonth
 ),
 AccumulatedTrades AS (
     SELECT
         CryptoTickerId,
+        CurrencySymbolId,
+        CurrencySymbol,
         TradeYear,
         TradeMonth,
         TradeSize,
         TradeValue,
-        SUM(TradeSize) OVER (PARTITION BY CryptoTickerId ORDER BY TradeYear, TradeMonth) AS AccumulatedTradeSize,
-        SUM(TradeValue) OVER (PARTITION BY CryptoTickerId ORDER BY TradeYear, TradeMonth) AS AccumulatedTradeValue
+        SUM(TradeSize) OVER (PARTITION BY CryptoTickerId, CurrencySymbolId ORDER BY TradeYear, TradeMonth) AS AccumulatedTradeSize,
+        SUM(TradeValue) OVER (PARTITION BY CryptoTickerId, CurrencySymbolId ORDER BY TradeYear, TradeMonth) AS AccumulatedTradeValue
     FROM
         AggregatedTrades
 )
 SELECT
     CryptoTickerId,
+    CurrencySymbolId,
+    CurrencySymbol,
     TradeYear,
     TradeMonth,
     AccumulatedTradeSize,
@@ -140,5 +198,5 @@ SELECT
 FROM
     AccumulatedTrades
 ORDER BY
-    CryptoTickerId, TradeYear, TradeMonth
-OPTION (MAXRECURSION 0);
+    CryptoTickerId, CurrencySymbolId, TradeYear, TradeMonth
+OPTION (MAXRECURSION 0)
