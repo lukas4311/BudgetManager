205d10bd4a1f893bff8f340db063f982605edc2e
Lukas Salficky <lukas4311@gmail.com>
2024-07-12 19:30:08 +0200

    Enum item service.

--- BudgetManager.Services.Contracts/IEnumService.cs
+++ BudgetManager.Services.Contracts/IEnumService.cs
@@ -1,10 +1,27 @@
 ﻿using BudgetManager.Data.DataModels;
 using BudgetManager.Domain.DTOs;
 using BudgetManager.Repository;
+using System.Collections.Generic;
 
 namespace BudgetManager.Services.Contracts
 {
+    /// <summary>
+    /// Service for enum items
+    /// </summary>
     public interface IEnumService : IBaseService<EnumItemModelAdjusted, EnumItem, IRepository<EnumItem>>
     {
+        /// <summary>
+        /// Get enum item by code
+        /// </summary>
+        /// <param name="enumItemCode">Enum item code</param>
+        /// <returns>Model of enum item</returns>
+        EnumItemModelAdjusted GetByCode(string enumItemCode);
+
+        /// <summary>
+        /// Get all enum items with specified enum type code
+        /// </summary>
+        /// <param name="enumItemTypeCode">Enum item type code</param>
+        /// <returns>All enum items with specified type code</returns>
+        IEnumerable<EnumItemModelAdjusted> GetAllByTypeCode(string enumItemTypeCode);
     }
 }
--- BudgetManager.Services/EnumService.cs
+++ BudgetManager.Services/EnumService.cs
@@ -4,18 +4,24 @@ using BudgetManager.Domain.DTOs;
 using BudgetManager.Repository;
 using BudgetManager.Services.Contracts;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace BudgetManager.Services
 {
-    
-
+    /// <inheritdoc/>
     public class EnumService : BaseService<EnumItemModelAdjusted, EnumItem, IRepository<EnumItem>>, IEnumService
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumService"/> class.
+        /// </summary>
+        /// <param name="repository">Enum item repository</param>
+        /// <param name="mapper">Automapper instance</param>
         public EnumService(IRepository<EnumItem> repository, IMapper mapper) : base(repository, mapper)
         {
         }
 
+        /// <inheritdoc/>
         public override EnumItemModelAdjusted Get(int id)
         {
             return repository.FindByCondition(p => p.Id == id)
@@ -31,6 +37,43 @@ namespace BudgetManager.Services
                     Id = e.Id
                 })
                 .SingleOrDefault();
+        }
+
+        /// <inheritdoc/>
+        public EnumItemModelAdjusted GetByCode(string enumItemCode)
+        {
+            return repository.FindAll()
+                .Where(p => p.Code == enumItemCode)
+                .Include(d => d.EnumItemType)
+                .Select(e => new EnumItemModelAdjusted
+                {
+                    Code = e.Code,
+                    Name = e.Name,
+                    EnumItemTypeId = e.EnumItemTypeId,
+                    EnumItemTypeCode = e.EnumItemType.Code,
+                    EnumItemTypeName = e.EnumItemType.Name,
+                    Metadata = e.Metadata,
+                    Id = e.Id
+                })
+                .SingleOrDefault();
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<EnumItemModelAdjusted> GetAllByTypeCode(string enumItemTypeCode)
+        {
+            return repository.FindAll()
+                .Include(d => d.EnumItemType)
+                .Where(p => p.EnumItemType.Code == enumItemTypeCode)
+                .Select(e => new EnumItemModelAdjusted
+                {
+                    Code = e.Code,
+                    Name = e.Name,
+                    EnumItemTypeId = e.EnumItemTypeId,
+                    EnumItemTypeCode = e.EnumItemType.Code,
+                    EnumItemTypeName = e.EnumItemType.Name,
+                    Metadata = e.Metadata,
+                    Id = e.Id
+                });
         }
     }
 }
