1f55f001f433575700d32e9323db9c443a79b09d
lukas4311 <lukas4311@gmail.com>
2024-08-12 17:48:38 +0200

    Figi and twelvedata

--- BudgetManager.WebScrapers/Services/TwelveDataService.py
+++ BudgetManager.WebScrapers/Services/TwelveDataService.py
@@ -1,7 +1,20 @@
+from typing import List
+
+from twelvedata import TDClient
+
+
 class TwelveDataService:
     logging: object
     apiToken: str
-    
-    def __init__(self, apiToken: str, logging:object):
+
+    def __init__(self, api_token: str, logging: object):
         self.logging = logging
-        self.token = apiToken
+        self.token = api_token
+        self.__twelve_data_client = TDClient(apikey=api_token)
+
+    def get_ticker_info(self, ticker: str):
+        self.__twelve_data_client.symbol_search(symbol=ticker)
+
+    def get_tickers_info(self, tickers: List[str]):
+        symbols = ','.join(tickers)
+        self.__twelve_data_client.symbol_search(symbol=symbols)
