9757b3a32c57dbd0a26a2f393002a3ff4e0a1f7a
Lukas Salficky <lukas4311@gmail.com>
2024-08-12 17:58:38 +0200

    Finnhub service to get symbol info from isin

--- BudgetManager.WebScrapers/Services/Finnhub.py
+++ BudgetManager.WebScrapers/Services/Finnhub.py
@@ -1,4 +1,21 @@
+from dataclasses import dataclass
+from typing import List
+from dacite import from_dict
 from finnhub import Client
+
+
+@dataclass
+class SymbolInfo:
+    description: str
+    displaySymbol: str
+    symbol: str
+    type: str
+
+
+@dataclass
+class StockResponse:
+    count: int
+    result: List[SymbolInfo]
 
 
 class FinnhubService:
@@ -6,10 +23,16 @@ class FinnhubService:
     def __init__(self, finnhub_client: Client):
         self.__finnhub_client = finnhub_client
 
-    def get_ticket_from_isin(self, isin: str) -> str | None:
+    def get_symbol_info_from_isin(self, isin: str) -> SymbolInfo | None:
         response = self.__finnhub_client.symbol_lookup(isin)
+        finnhub_symbol_model: StockResponse = from_dict(data_class=StockResponse, data=response)
 
-        if response.count == 0:
+        if finnhub_symbol_model.count == 0:
             return None
 
-        return response.result[0]
+        return finnhub_symbol_model.result[0]
+
+
+# finnhub = FinnhubService(Client('c8ua7kaad3ibddueerq0'))
+# data = finnhub.get_ticker_from_isin('IE00B3XXRP09')
+# print(data.symbol, data.displaySymbol)
