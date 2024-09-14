afce68d19aab635909258995ab21630f61cbc53d
Lukas Salficky <lukas4311@gmail.com>
2024-09-14 08:33:05 +0200

    Configured tailwind and add utils.ts for shadcn

--- ManagerWeb/tailwind.config.js
+++ ManagerWeb/tailwind.config.js
@@ -1,4 +1,7 @@
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
 module.exports = {
+    darkMode: ["class"],
     content: ["./Typescript/**/*.jsx", "./Typescript/**/*.tsx", "./Typescript/**/*.ts", "./Typescript/**/*.js"],
     important: true,
     theme: {
@@ -11,7 +14,40 @@ module.exports = {
                 vermilion: "#e03d15ff",
                 battleshipGrey: "#707F99ff",
                 rufous: "#b00000ff",
-                lightGray: "#A2ACBDff"
+                lightGray: "#A2ACBDff",
+                border: "hsl(var(--border))",
+                input: "hsl(var(--input))",
+                ring: "hsl(var(--ring))",
+                background: "hsl(var(--background))",
+                foreground: "hsl(var(--foreground))",
+                primary: {
+                    DEFAULT: "hsl(var(--primary))",
+                    foreground: "hsl(var(--primary-foreground))",
+                },
+                secondary: {
+                    DEFAULT: "hsl(var(--secondary))",
+                    foreground: "hsl(var(--secondary-foreground))",
+                },
+                destructive: {
+                    DEFAULT: "hsl(var(--destructive))",
+                    foreground: "hsl(var(--destructive-foreground))",
+                },
+                muted: {
+                    DEFAULT: "hsl(var(--muted))",
+                    foreground: "hsl(var(--muted-foreground))",
+                },
+                accent: {
+                    DEFAULT: "hsl(var(--accent))",
+                    foreground: "hsl(var(--accent-foreground))",
+                },
+                popover: {
+                    DEFAULT: "hsl(var(--popover))",
+                    foreground: "hsl(var(--popover-foreground))",
+                },
+                card: {
+                    DEFAULT: "hsl(var(--card))",
+                    foreground: "hsl(var(--card-foreground))",
+                },
             },
             width: {
                 '1/10': '10%',
@@ -28,9 +64,31 @@ module.exports = {
             },
             screens: {
                 '3xl': '1800px',
-            }
+            },
+            borderRadius: {
+                lg: `var(--radius)`,
+                md: `calc(var(--radius) - 2px)`,
+                sm: "calc(var(--radius) - 4px)",
+            },
+            fontFamily: {
+                sans: ["var(--font-sans)", ...fontFamily.sans],
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: { height: "0" },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: "0" },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+            },
         },
     },
     variants: {},
-    plugins: [],
+    plugins: [require("tailwindcss-animate")],
 }
