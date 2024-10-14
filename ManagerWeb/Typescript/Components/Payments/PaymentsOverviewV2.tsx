5056794f031efa0ae9286c4829b7a452a0fd7502
Lukas Salficky <lukas4311@gmail.com>
2024-10-14 18:09:02 +0200

    Stats of payments

--- ManagerWeb/Typescript/Components/Payments/PaymentsOverviewV2.tsx
+++ ManagerWeb/Typescript/Components/Payments/PaymentsOverviewV2.tsx
@@ -234,8 +234,8 @@ const IncomeCard = (props: IncomeExpenseCardProps) => {
 }
 
 const ExpenseCard = (props: IncomeExpenseCardProps) => {
-    const revenues = props.payments.filter(p => p.paymentTypeCode == "Expense");
-    const sum = _.sumBy(revenues, p => p.amount);
+    const expense = props.payments.filter(p => p.paymentTypeCode == "Expense");
+    const sum = _.sumBy(expense, p => p.amount);
 
     return (
         <div className={`flex flex-col bg-battleshipGrey px-4 py-6 rounded-lg relative ${props?.cardClass ?? ""}`}>
@@ -387,8 +387,26 @@ class PaymentsStatsProps {
 }
 
 const PaymentsStats = (props: PaymentsStatsProps) => {
+    let sumRevenues = 0
+    let sumExpense = 0
+    let sumSaved = 0
+    let months = 1
 
-    
+    if (props.payments.length > 0) {
+        const minDate = moment(props.payments[0].date);
+        const maxDate = moment(props.payments[props.payments.length - 1].date);
+        months = minDate.diff(maxDate, 'months');
+
+        if (months == 0)
+            months = 1
+
+        const revenues = props.payments.filter(p => p.paymentTypeCode == "Revenue");
+        sumRevenues = _.sumBy(revenues, p => p.amount);
+
+        const expense = props.payments.filter(p => p.paymentTypeCode == "Expense");
+        sumExpense = _.sumBy(expense, p => p.amount);
+        sumSaved = sumRevenues - sumExpense;
+    }
 
     return (
         <div>
@@ -396,11 +414,11 @@ const PaymentsStats = (props: PaymentsStatsProps) => {
 
             <div className='grid grid-cols-2 gap-y-5 text-left text-2xl'>
                 <p>Monthly average income</p>
-                <p>50,-</p>
+                <p className='font-bold'>{(sumRevenues / months).toFixed(0)}</p>
                 <p>Monthly average expense</p>
-                <p>50,-</p>
+                <p className='font-bold'>{(sumExpense / months).toFixed(0)}</p>
                 <p>Monthly average saved</p>
-                <p>50,-</p>
+                <p className='font-bold'>{(sumSaved / months).toFixed(0)}</p>
             </div>
         </div>
     );
