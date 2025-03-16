baab3ddc8f5961f59e4875271fc5e6dbe8f2aa7d
Lukas Salficky <lukas4311@gmail.com>
2025-03-16 15:54:06 +0100

    Add doucmentry comments

--- ManagerWeb/Services/IUserService.cs
+++ ManagerWeb/Services/IUserService.cs
@@ -3,12 +3,32 @@ using BudgetManager.ManagerWeb.Models;
 
 namespace BudgetManager.ManagerWeb.Services
 {
+    /// <summary>
+    /// Provides user-related services such as authentication and sign-in.
+    /// </summary>
     public interface IUserService
     {
+        /// <summary>
+        /// Authenticates the user with the given username and password.
+        /// </summary>
+        /// <param name="username">The username of the user.</param>
+        /// <param name="password">The password of the user.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the authenticated user model.</returns>
         Task<UserModel> Authenticate(string username, string password);
 
+        /// <summary>
+        /// Signs in the user with the specified login and user ID.
+        /// </summary>
+        /// <param name="login">The login of the user.</param>
+        /// <param name="userId">The ID of the user.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         Task SignIn(string login, int userId);
 
+        /// <summary>
+        /// Gets the user ID for the specified user login.
+        /// </summary>
+        /// <param name="userLogin">The login of the user.</param>
+        /// <returns>The ID of the user.</returns>
         int GetUserId(string userLogin);
     }
 }
--- ManagerWeb/Services/UserService.cs
+++ ManagerWeb/Services/UserService.cs
@@ -11,35 +11,44 @@ using BudgetManager.Data.DataModels;
 
 namespace BudgetManager.ManagerWeb.Services
 {
+    /// <inheritdoc />
     internal class UserService : IUserService
     {
         private readonly IRepository<UserIdentity> userIdentityRepository;
         private readonly IHttpContextAccessor httpContextAccessor;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserService"/> class.
+        /// </summary>
+        /// <param name="userIdentityRepository">The repository for user identity data.</param>
+        /// <param name="httpContextAccessor">The HTTP context accessor.</param>
         public UserService(IRepository<UserIdentity> userIdentityRepository, IHttpContextAccessor httpContextAccessor)
         {
             this.userIdentityRepository = userIdentityRepository;
             this.httpContextAccessor = httpContextAccessor;
         }
 
+        /// <inheritdoc />
         public async Task<UserModel> Authenticate(string username, string passwordHash)
         {
             return await Task.Run(() => this.userIdentityRepository.FindByCondition(x => x.Login == username && x.PasswordHash == passwordHash)
                         .Select(a => new UserModel { Login = a.Login }).SingleOrDefault()).ConfigureAwait(false);
         }
 
+        /// <inheritdoc />
         public int GetUserId(string userLogin)
         {
             return this.userIdentityRepository.FindByCondition(a => a.Login == userLogin).Single().Id;
         }
 
+        /// <inheritdoc />
         public async Task SignIn(string login, int userId)
         {
             List<Claim> claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
-                new Claim(ClaimTypes.Name, login)
-            };
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+                        new Claim(ClaimTypes.Name, login)
+                    };
 
             ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
             ClaimsPrincipal principal = new ClaimsPrincipal(identity);
