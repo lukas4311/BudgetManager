a23003799d529bda0221d7d57d9ae50df4b1c4d6
Lukas Salficky <lukas4311@gmail.com>
2025-05-17 19:10:43 +0200

    Add comments and return types.

--- BudgetManager.WebScrapers/Services/AlphaVantage.py
+++ BudgetManager.WebScrapers/Services/AlphaVantage.py
@@ -5,15 +5,32 @@ from config import alphavantageUrl
 
 
 class AlphaVantageService:
+    # Keys used to extract relevant data from the Alpha Vantage API response
     timeSeriesKey = "Time Series (Daily)"
     closeValueKey = "4. close"
     token: str
 
     def __init__(self, api_token: str):
+        """
+        Initializes the AlphaVantageService with the given API token.
+
+        Args:
+            api_token (str): API key for authenticating with Alpha Vantage.
+        """
         self.token = api_token
 
-    def get_stock_price_history(self, ticker: str):
+    def get_stock_price_history(self, ticker: str) -> list[StockPriceData]:
+        """
+        Retrieves the historical daily stock prices for a given ticker.
+
+        Args:
+            ticker (str): The stock ticker symbol (e.g., 'AAPL').
+
+        Returns:
+            list[StockPriceData]: List of StockPriceData objects with date and close price.
+        """
         print('Downloading ' + ticker)
+
         url = f'{alphavantageUrl}/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={ticker}&apikey={self.token}&outputsize=full'
         r = requests.get(url)
         data = r.json()
@@ -24,7 +41,10 @@ class AlphaVantageService:
             pandas_date = pd.to_datetime(key)
             pandas_date = pandas_date.tz_localize("Europe/Prague")
             pandas_date = pandas_date.tz_convert("utc")
-            price_model = StockPriceData(pandas_date, float(time_series_data[key][self.closeValueKey]))
+            price_model = StockPriceData(
+                pandas_date,
+                float(time_series_data[key][self.closeValueKey])
+            )
             stock_price_data.append(price_model)
 
         return stock_price_data
--- BudgetManager.WebScrapers/Services/CryptoRepository.py
+++ BudgetManager.WebScrapers/Services/CryptoRepository.py
@@ -1,7 +1,7 @@
 from typing import List
 import pandas as pd
 import pyodbc
-from sqlalchemy import create_engine, select, update, and_
+from sqlalchemy import create_engine, select, update, and_, Sequence
 from sqlalchemy.orm import Session
 import secret
 from Models.CoinbaseReportData import CoinbaseReportData
@@ -13,14 +13,34 @@ from config import dbConnectionString
 
 
 class CryptoRepository:
-    def get_ticker_id(self, ticker: str):
+    """
+    Repository class for handling operations related to cryptocurrency trades,
+    tickers, and broker reports in the database.
+    """
+
+    def get_ticker_id(self, ticker: str) -> int | None:
+        """
+        Retrieves the ID of a crypto ticker from the database.
+
+        Args:
+            ticker (str): The ticker symbol (e.g., 'BTC').
+
+        Returns:
+            int | None: The ID of the ticker if it exists, otherwise None.
+        """
         conn = pyodbc.connect(dbConnectionString.format(serverName=secret.serverName, datebaseName=secret.datebaseName))
         stock_ticker_sql = """SELECT [Id] FROM [dbo].[CryptoTicker] WHERE [Ticker] = ?"""
         ticker_data = pd.read_sql_query(stock_ticker_sql, conn, params=[ticker])
         conn.close()
         return ticker_data["Id"].values[0] if ticker_data["Id"].values.size > 0 else None
 
-    def create_new_ticker(self, ticker: str):
+    def create_new_ticker(self, ticker: str) -> None:
+        """
+        Inserts a new crypto ticker into the database.
+
+        Args:
+            ticker (str): The ticker symbol to insert (e.g., 'BTC').
+        """
         conn = pyodbc.connect(dbConnectionString.format(serverName=secret.serverName, datebaseName=secret.datebaseName))
         cursor = conn.cursor()
         params = (ticker, ticker)
@@ -28,15 +48,29 @@ class CryptoRepository:
         conn.commit()
         conn.close()
 
-    def get_currency_id(self, currency: str):
+    def get_currency_id(self, currency: str) -> int | None:
+        """
+        Retrieves the ID of a currency from the database.
+
+        Args:
+            currency (str): The currency symbol (e.g., 'USD').
+
+        Returns:
+            int | None: The ID of the currency if it exists, otherwise None.
+        """
         conn = pyodbc.connect(dbConnectionString.format(serverName=secret.serverName, datebaseName=secret.datebaseName))
         currency_sql = """SELECT Id from CurrencySymbol where SYMBOL = ?"""
         currency_data = pd.read_sql_query(currency_sql, conn, params=[currency])
         conn.close()
-
         return currency_data["Id"].values[0] if currency_data["Id"].values.size > 0 else None
 
-    def get_all_crypto_broker_reports_to_process(self):
+    def get_all_crypto_broker_reports_to_process(self) -> Sequence[BrokerReportToProcess]:
+        """
+        Retrieves all broker reports of type 'Crypto' that are currently in 'InProcess' state.
+
+        Returns:
+            Sequence[BrokerReportToProcess]: List of matching broker reports.
+        """
         engine = create_engine(
             f'mssql+pyodbc://@{secret.serverName}/{secret.datebaseName}?driver=ODBC+Driver+17+for+SQL+Server&Trusted_Connection=yes')
 
@@ -61,7 +95,13 @@ class CryptoRepository:
 
         return broker_report_data
 
-    def insert_crypto_trade(self, tradingData: CoinbaseReportData):
+    def insert_crypto_trade(self, tradingData: CoinbaseReportData) -> None:
+        """
+        Inserts a new crypto trade into the database if it does not already exist.
+
+        Args:
+            tradingData (CoinbaseReportData): The data of the trade to insert.
+        """
         print("Insert record")
 
         ticker_id = int(self.get_ticker_id(tradingData.ticker))
@@ -97,7 +137,13 @@ class CryptoRepository:
         else:
             print('Trade is already saved.')
 
-    def store_trade_data(self, crypto_trade_data: List[CoinbaseReportData]):
+    def store_trade_data(self, crypto_trade_data: List[CoinbaseReportData]) -> None:
+        """
+        Processes and stores a list of crypto trade data entries.
+
+        Args:
+            crypto_trade_data (List[CoinbaseReportData]): List of trade entries to store.
+        """
         for trade in crypto_trade_data:
             ticker_id = self.get_ticker_id(trade.ticker)
 
@@ -106,7 +152,14 @@ class CryptoRepository:
 
             self.insert_crypto_trade(trade)
 
-    def change_process_state(self, broker_report_id: int, state_code: str):
+    def change_process_state(self, broker_report_id: int, state_code: str) -> None:
+        """
+        Updates the processing state of a broker report.
+
+        Args:
+            broker_report_id (int): ID of the broker report.
+            state_code (str): New state code to assign (e.g., 'Processed').
+        """
         engine = create_engine(
             f'mssql+pyodbc://@{secret.serverName}/{secret.datebaseName}?driver=ODBC+Driver+17+for+SQL+Server&Trusted_Connection=yes')
 
